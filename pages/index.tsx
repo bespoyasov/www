4e36bb855d914c9077e22d0529f34e698e8a7cba
Alexander Bespoyasov <forjob159@gmail.com>
2020-07-07 12:48:09 +0300

    Add simple page

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+import { Center } from "../components/Center";
+
+const Main = () => (
+  <main>
+    <Head>
+      <title>АБ</title>
+    </Head>
+    <Center>
+      <h1>Hello</h1>
+    </Center>
+  </main>
+);
+
+export default Main;
