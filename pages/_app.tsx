1e1b3a016b3923bbff7328944f8d7f342b0b83b2
Alexander Bespoyasov <forjob159@gmail.com>
2020-07-08 12:48:20 +0300

    Add sizes settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import "@shared/styles/typography.css";
 import "@shared/styles/palette.css";
+import "@shared/styles/sizes.css";
 import "@shared/styles/grid.css";
 
 import "@shared/styles/theme.css";
