b39655c5b5e3455418de2d632dc44df00e69e7c6
Alexander Bespoyasov <forjob159@gmail.com>
2020-07-08 14:15:31 +0300

    Cleanup main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import Head from "next/head";
-import { Center } from "@components/Center";
 
 const Main = () => (
   <main>
     <Head>
       <title>АБ</title>
     </Head>
-    <Center>
-      <h1>Hello</h1>
-    </Center>
   </main>
 );
 
