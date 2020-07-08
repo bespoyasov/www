b8cb449f01026bed88211527eff55b7d264834ee
Alexander Bespoyasov <forjob159@gmail.com>
2020-07-08 16:22:34 +0300

    Add `About` component in `Header` on main page

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,15 +1,22 @@
 import React from "react";
-import { Logo } from "@components/Logo";
+import { useRouter } from "next/router";
+import { onMainPage } from "@shared/onMainPage";
 import { Nav } from "@components/Nav";
+import { Logo } from "@components/Logo";
+import { About } from "@components/About";
 import styles from "./Header.module.css";
 
 export const Header: React.FC = () => {
+  const { pathname } = useRouter();
+
   return (
     <header className={styles.header}>
       <div>
-        <Logo />
+        <Logo className={styles.logo} />
         <Nav className={styles.nav} />
       </div>
+
+      {onMainPage(pathname) && <About />}
     </header>
   );
 };
