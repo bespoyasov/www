f2c4bedffa1a697cd29f8bd940b5c87c2942e582
Alexander Bespoyasov <forjob159@gmail.com>
2020-07-08 14:12:23 +0300

    Add `Footer` component

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Copyright } from "@components/Copyright";
+import { Socials } from "@components/Socials";
+import { Person } from "@components/MicroData";
+import styles from "./Footer.module.css";
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <Copyright />
+      <Socials />
+      <Person />
+    </footer>
+  );
+};
