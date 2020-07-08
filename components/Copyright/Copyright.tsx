574385d162e94462c9576a000ce5f4f4cb0894aa
Alexander Bespoyasov <forjob159@gmail.com>
2020-07-08 13:12:53 +0300

    Add `Copyright` component

---
+++ components/Copyright/Copyright.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Copyright.module.css";
+
+export const Copyright: React.FC = () => {
+  return (
+    <div>
+      <span className={styles.dates}>2010+</span>
+      <a href="mailto:bespoyasov@me.com" className="text-color">
+        bespoyasov@me.com
+      </a>
+    </div>
+  );
+};
