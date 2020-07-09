f7d064c1b2525ae848790ead795aa8eb1d9e8332
Alexander Bespoyasov <forjob159@gmail.com>
2020-07-09 13:15:55 +0300

    Render `Metadata` in `Post`

--- layouts/Post/Post.tsx
+++ layouts/Post/Post.tsx
@@ -1,25 +1,28 @@
 import React from "react";
 import Head from "next/head";
 import { MDXProvider } from "@mdx-js/react";
-import { Metadata } from "@domain/metadata/types";
+import { Metadata as MetadataType } from "@domain/metadata/types";
 import { Blockquote } from "@components/Blockquote";
+import { Metadata } from "@components/Metadata";
 import styles from "./Post.module.css";
 
 type Props = {
-  metadata: Metadata;
+  metadata: MetadataType;
 };
 
 export const PostLayout: React.FC<Props> = ({ metadata, children }) => {
   const { title, description } = metadata;
 
   return (
-    <main className={styles.post}>
+    <MDXProvider components={{ blockquote: Blockquote }}>
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
       </Head>
-
-      <MDXProvider components={{ blockquote: Blockquote }}>{children}</MDXProvider>
-    </main>
+      <div className={styles.post}>
+        <main>{children}</main>
+        <Metadata metadata={metadata} />
+      </div>
+    </MDXProvider>
   );
 };
