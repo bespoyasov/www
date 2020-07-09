1438f1e43b3b5b1b16c8842f9d76710dfbbabf33
Alexander Bespoyasov <forjob159@gmail.com>
2020-07-09 12:48:14 +0300

    Extract `Tags` into domain

---
+++ domain/tags/types.ts
@@ -0,0 +1,54 @@
+export type Tags = keyof Tag;
+export enum Tag {
+  Advice = "Совет",
+  Ai = "Искусственный интеллект",
+  A11y = "Доступность",
+
+  Behavior = "Поведение",
+  Biology = "Биология",
+  Books = "Книги",
+  Brain = "Мозг",
+  Butthurt = "Бомбит",
+
+  Cap = "Капитан очевидность",
+  Communication = "Общение",
+
+  Dev = "Разработка",
+  Design = "Дизайн",
+
+  Economics = "Экономика",
+  Editorial = "Выбор редакции",
+  Emotions = "Эмоции",
+
+  History = "История",
+
+  Learning = "Обучение",
+  Links = "Ссылки",
+
+  Math = "Математика",
+  Management = "Управление",
+  Music = "Музыка",
+
+  Opinion = "Ящитаю",
+
+  Philosophy = "Философия",
+  Physics = "Физика",
+  Process = "Как это было",
+  Product = "Продукт",
+  Productivity = "Продуктивность",
+  Psychology = "Психология",
+
+  Reading = "Чтение",
+  Reflection = "Рефлексия",
+  Running = "Пробежки",
+
+  Space = "Космос",
+  Story = "Офигительная история",
+  Summary = "Конспект",
+
+  Text = "Редактура",
+  Tools = "Инструменты",
+  Travel = "Мир",
+
+  Urban = "Урбанистика",
+}
