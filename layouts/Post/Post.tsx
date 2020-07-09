afd28a3e742257cdb7fe366a63d4424adc828f1f
Alexander Bespoyasov <forjob159@gmail.com>
2020-07-09 12:05:44 +0300

    Enrich `Post` with title and description

--- layouts/Post/Post.tsx
+++ layouts/Post/Post.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import Head from "next/head";
 import { MDXProvider } from "@mdx-js/react";
 import { Blockquote } from "@components/Blockquote";
+import { PostMetadata } from "./types";
 import styles from "./Post.module.css";
 
-export const PostLayout: React.FC = ({ children }) => {
+type Props = {
+  metadata: PostMetadata;
+};
+
+export const PostLayout: React.FC<Props> = ({ metadata, children }) => {
+  const { title, description } = metadata;
+
   return (
     <main className={styles.post}>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Head>
+
       <MDXProvider components={{ blockquote: Blockquote }}>{children}</MDXProvider>
     </main>
   );
