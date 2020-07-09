7cb1a4a678e1987e41fd6b21dc6d867c0e651221
Alexander Bespoyasov <forjob159@gmail.com>
2020-07-09 14:05:44 +0300

    Render `Tags` in `Metadata`

--- components/Metadata/Metadata.tsx
+++ components/Metadata/Metadata.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Metadata as MetadataType } from "@domain/metadata/types";
+import { Tags } from "@components/Tags";
 import { toLocalizedDateString } from "./toLocalizedDateString";
 import styles from "./Metadata.module.css";
 
@@ -8,11 +9,12 @@ type Props = {
 };
 
 export const Metadata: React.FC<Props> = ({ metadata }) => {
-  const { datetime } = metadata;
+  const { datetime, tags } = metadata;
 
   return (
     <footer className={styles.metadata}>
       <time dateTime={datetime}>{toLocalizedDateString(datetime)}</time>
+      {!!tags && <Tags tags={tags} />}
     </footer>
   );
 };
