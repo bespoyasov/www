150505d6b131810c5de6268d92f7fda0e87dd3a6
Alexander Bespoyasov <forjob159@gmail.com>
2020-07-15 17:19:20 +0300

    Add `Nullable` and `Optional` kind types

--- shared/types.ts
+++ shared/types.ts
@@ -7,4 +7,7 @@ export type UrlSlug = string;
 export type List<TEntity> = TEntity[];
 export type Dict<TValue> = Record<string, TValue>;
 
+export type Nullable<TEntity> = TEntity | null;
+export type Optional<TEntity> = TEntity | undefined;
+
 export type AnyObject = Dict<unknown>;
