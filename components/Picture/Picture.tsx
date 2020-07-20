7d54a782f7937b1f65e199965e6e5efcb7519aef
Alexander Bespoyasov <forjob159@gmail.com>
2020-07-20 16:27:31 +0300

    Add citation source property for `Picture` component

--- components/Picture/Picture.tsx
+++ components/Picture/Picture.tsx
@@ -1,24 +1,29 @@
 import React from "react";
-import { UrlSlug } from "@shared/types";
+import { UrlSlug, AbsoluteUrl } from "@shared/types";
 import styles from "./Picture.module.css";
 
 type ImageRelativePath = UrlSlug;
 type AlternativeText = string;
 type VisibleCaption = string;
+type CitationSource = AbsoluteUrl;
 
 type Props = {
   src: ImageRelativePath;
   alt: AlternativeText;
   caption?: VisibleCaption;
+  cite?: CitationSource;
 };
 
-export const Picture: React.FC<Props> = ({ src, alt, caption, children }) => {
+export const Picture: React.FC<Props> = ({ src, alt, cite, caption }) => {
+  const Container = cite ? "a" : React.Fragment;
+
   return (
     <figure className={styles.picture}>
-      <picture>
-        <img src={src} alt={alt} />
-        {children}
-      </picture>
+      <Container href={cite}>
+        <picture>
+          <img src={src} alt={alt} />
+        </picture>
+      </Container>
       {!!caption && <figcaption>{caption}</figcaption>}
     </figure>
   );
