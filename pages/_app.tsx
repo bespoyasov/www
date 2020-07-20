143f6fe8773dec7c83bafc6fd83a7bb30f7b09b9
Alexander Bespoyasov <forjob159@gmail.com>
2020-07-20 16:26:58 +0300

    Add a module for animation declarations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import "@shared/styles/palette.css";
 import "@shared/styles/sizes.css";
 import "@shared/styles/grid.css";
 
+import "@shared/styles/animation.css";
 import "@shared/styles/theme.css";
 import "@shared/styles/global.css";
 import "@shared/styles/rhythm.css";
