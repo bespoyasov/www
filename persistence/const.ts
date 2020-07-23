f9c6003a2431eeb6ed234766dca9bc609dfd34b6
Alexander Bespoyasov <forjob159@gmail.com>
2020-07-23 11:02:13 +0300

    Move path constants from `server` to `persistence`

---
+++ persistence/const.ts
@@ -0,0 +1,5 @@
+import path from "path";
+
+export const ROOT_DIRECTORY = process.cwd();
+export const BLOG_DIRECTORY = path.join(ROOT_DIRECTORY, "pages/blog");
+export const PROJECTS_DIRECTORY = path.join(ROOT_DIRECTORY, "pages/projects");
--- server/const.ts
+++
@@ -1,4 +0,0 @@
-import path from "path";
-
-export const ROOT_DIRECTORY = process.cwd();
-export const POSTS_DIRECTORY = path.join(ROOT_DIRECTORY, "pages/blog");
