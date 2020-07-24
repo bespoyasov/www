85566900c4c455b2f30da9e541e187a89cf3eabe
Alexander Bespoyasov <forjob159@gmail.com>
2020-07-24 19:26:51 +0300

    Introduce `Unique` type

--- shared/types.ts
+++ shared/types.ts
@@ -10,5 +10,6 @@ export type Dict<TValue> = Record<string, TValue>;
 
 export type Nullable<TEntity> = TEntity | null;
 export type Optional<TEntity> = TEntity | undefined;
+export type Unique<TEntity> = TEntity;
 
 export type AnyObject = Dict<unknown>;
