921aef1e46c7a3633948bb43f66577771e5a900b
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-03 18:37:27 +0300

    Move an `MDXProvider` to a `Post` layout component

--- layouts/Post/Post.tsx
+++ layouts/Post/Post.tsx
@@ -1,6 +1,23 @@
 import React from "react";
+import { MDXProvider } from "@mdx-js/react";
+
+import { Blockquote } from "@components/Blockquote";
+import { Highlight } from "@components/Highlight";
+import { LinkProxy } from "@components/LinkProxy";
+import { Picture } from "@components/Picture";
 import styles from "./Post.module.css";
 
+const substitutes = {
+  blockquote: Blockquote,
+  mark: Highlight,
+  img: Picture,
+  a: LinkProxy,
+};
+
 export const PostLayout: React.FC = ({ children }) => {
-  return <div className={styles.post}>{children}</div>;
+  return (
+    <MDXProvider components={substitutes}>
+      <div className={styles.post}>{children}</div>
+    </MDXProvider>
+  );
 };
--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -3,19 +3,14 @@ import Head from "next/head";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import { GetStaticProps, GetStaticPaths } from "next";
-import { MDXProvider } from "@mdx-js/react";
 
 import { Nullable } from "@shared/types";
 import { Metadata as MetadataType } from "@domain/metadata";
 import { projectsMetadata, projectsNames } from "@api/fetch";
 
 import { PostLayout } from "@layouts/Post";
-import { Picture } from "@components/Picture";
 import { Metadata } from "@components/Metadata";
 import { Adjacent } from "@components/Adjacent";
-import { Highlight } from "@components/Highlight";
-import { LinkProxy } from "@components/LinkProxy";
-import { Blockquote } from "@components/Blockquote";
 import { Description } from "@components/Description";
 
 type ProjectProps = {
@@ -44,32 +39,23 @@ export const getStaticPaths: GetStaticPaths = async () => {
   return { paths, fallback: false };
 };
 
-const substitutes = {
-  blockquote: Blockquote,
-  mark: Highlight,
-  img: Picture,
-  a: LinkProxy,
-};
-
 const Project: React.FC<ProjectProps> = ({ metadata, prevPost, nextPost }) => {
   const { title, description } = metadata;
   const { query } = useRouter();
   const PostContents = dynamic(() => import(`./${query.id}.mdx`));
 
   return (
-    <MDXProvider components={substitutes}>
+    <PostLayout>
       <Head>
         <title>{title}</title>
         <Description>{description}</Description>
       </Head>
-      <PostLayout>
-        <main>
-          <PostContents />
-        </main>
-        <Metadata metadata={metadata} />
-        <Adjacent prev={prevPost} next={nextPost} />
-      </PostLayout>
-    </MDXProvider>
+      <main>
+        <PostContents />
+      </main>
+      <Metadata metadata={metadata} />
+      <Adjacent prev={prevPost} next={nextPost} />
+    </PostLayout>
   );
 };
 
