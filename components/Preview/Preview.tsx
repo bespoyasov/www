3806de57429d29b264f5e4efd3374ab75e029fd6
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-03 16:04:41 +0300

    `ProjectPreview` => `Preview`

--- components/Preview/Preview.tsx
+++ components/Preview/Preview.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import Link from "next/link";
 import { Metadata } from "@domain/metadata";
 
-type ProjectPreviewProps = {
+type PreviewProps = {
   metadata: Metadata;
 };
 
-export const ProjectPreview: React.FC<ProjectPreviewProps> = ({ metadata }) => {
+export const Preview: React.FC<PreviewProps> = ({ metadata }) => {
   const { slug, title, description } = metadata;
 
   return (
     <div>
-      <h3>
+      <h2>
         <Link href={slug} as={slug}>
           <a className="text-color">{title}</a>
         </Link>
-      </h3>
+      </h2>
       <p>{description}</p>
     </div>
   );
