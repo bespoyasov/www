a7650e2be6c0b9ecdd39111f37ccc755f4392a27
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-03 15:04:08 +0300

    Introduce `activeIf` helper function to assign an active class name for a link when it's active

---
+++ components/Nav/activeIf.ts
@@ -0,0 +1,8 @@
+import { ClassName } from "@shared/types";
+import { classes } from "@shared/classes";
+
+type ActiveLinkClassName = ClassName;
+
+export function activeIf(condition: boolean): ActiveLinkClassName {
+  return classes(condition && "text-color");
+}
