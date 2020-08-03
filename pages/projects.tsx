f5d14db75f66e356ee2c35ea3cac38620caee8bd
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-03 15:26:53 +0300

    Add a projects page draft

---
+++ pages/projects.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Head from "next/head";
+import { GetStaticProps } from "next";
+
+import { Description } from "@components/Description";
+import { Showcase } from "@components/Showcase";
+
+import { Metadata } from "@domain/metadata";
+import { projectsMetadata } from "@api/fetch";
+
+type ProjectsProps = {
+  projects: Metadata[];
+};
+
+export const getStaticProps: GetStaticProps<ProjectsProps> = async () => {
+  return {
+    props: {
+      projects: await projectsMetadata(),
+    },
+  };
+};
+
+const Projects: React.FC<ProjectsProps> = ({ projects }) => {
+  return (
+    <>
+      <Head>
+        <title>Сделал</title>
+        <Description>Что я наделал.</Description>
+      </Head>
+      <Showcase projects={projects} />
+    </>
+  );
+};
+
+export default Projects;
