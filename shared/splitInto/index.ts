5507f44612bdbf72da5cbe15a25801bdca68d121
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-07 00:12:55 +0300

    Add a `splitInto` utility function for dividing arrays into determined amount of chunks

---
+++ shared/splitInto/index.ts
@@ -0,0 +1,12 @@
+import { AnyList, List } from "@shared/types";
+import { sizeOf } from "@shared/sizeOf";
+import { chunk } from "@shared/chunk";
+
+type PartsCount = number;
+
+export function splitInto<TList extends AnyList>(list: TList, parts: PartsCount = 1): List<TList> {
+  if (parts < 1) throw new Error("Cannot split an array into fewer than 1 part.");
+
+  const chunkSize = Math.ceil(sizeOf(list) / parts);
+  return chunk(list, chunkSize);
+}
