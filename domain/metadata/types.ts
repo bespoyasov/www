2ab6d2ea6dbb509c30f0e82f3aaa5ffd11903fe3
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-10 22:02:29 +0300

    Extend `Metadata` type with a `preview` field

--- domain/metadata/types.ts
+++ domain/metadata/types.ts
@@ -1,13 +1,16 @@
-import { List, Unique, DateTimeIsoString, UrlSlug } from "@shared/types";
+import { List, Unique, DateTimeIsoString, UrlSlug, RelativePath } from "@shared/types";
 import { TagKind } from "@domain/tags";
 
 type Title = string;
 type Description = string;
+type PreviewImage = RelativePath;
 
 export type Metadata = {
   title: Title;
   description: Description;
   datetime: DateTimeIsoString;
   slug: Unique<UrlSlug>;
+
   tags?: List<TagKind>;
+  preview?: PreviewImage;
 };
