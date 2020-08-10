6199a0a58820785435ba8aa19d54701d44ee6c37
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-10 22:02:54 +0300

    Add a cover to a `Preview` component

--- components/Preview/Preview.tsx
+++ components/Preview/Preview.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Link from "next/link";
 import { Metadata } from "@domain/metadata";
+import styles from "./Preview.module.css";
 
 type PreviewProps = {
   metadata: Metadata;
 };
 
 export const Preview: React.FC<PreviewProps> = ({ metadata }) => {
-  const { slug, title, description } = metadata;
+  const { slug, title, description, preview: cover } = metadata;
 
   return (
-    <div>
+    <div className={styles.preview}>
+      {!!cover && <img src={cover} alt={title} />}
       <h2>
         <Link href={slug} as={slug}>
           <a className="text-color">{title}</a>
