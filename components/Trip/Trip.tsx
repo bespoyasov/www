f577ae9de20889670b0e74d63c1437205b37c629
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-16 11:50:32 +0300

    Add a `Trip` component

---
+++ components/Trip/Trip.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+import { Metadata } from "@domain/metadata";
+import { Cover } from "@components/Cover";
+import styles from "./Trip.module.css";
+
+type TripProps = {
+  place: Metadata;
+};
+
+export const Trip: React.FC<TripProps> = ({ place }) => {
+  const { title, description, slug } = place;
+
+  return (
+    <article className={styles.trip}>
+      <Link href={slug}>
+        <a className="text-color">
+          <div className={styles.cover}>
+            <Cover for={place} withExtension="jpg" />
+          </div>
+          <h2>{title}</h2>
+        </a>
+      </Link>
+      <p>{description}</p>
+    </article>
+  );
+};
