685e499b41ba92d48aabbcbd06eb075813fd43a8
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-17 21:30:42 +0300

    Add a filter by tags utility function

---
+++ shared/filter/withTag.ts
@@ -0,0 +1,11 @@
+import { Metadata } from "@domain/metadata";
+import { TagKind } from "@domain/tags";
+import { FilterFunction } from "./types";
+
+export function withTag(tag: TagKind): FilterFunction<Metadata> {
+  return function applyFilter(entity) {
+    return entity.tags.includes(tag);
+  };
+}
+
+export const withDevTag = withTag("Dev");
