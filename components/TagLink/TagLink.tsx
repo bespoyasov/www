614ab571e360e68f3c2bfe420edbef7df5ceb0f6
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-18 17:31:07 +0300

    Replace tag's value determination implementation with a utility function

--- components/TagLink/TagLink.tsx
+++ components/TagLink/TagLink.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { Tag as TagEnum, TagKind } from "@domain/tags";
+import { TagKind, valueOf } from "@domain/tags";
 import { isStatic } from "./isStatic";
 
 type TagLinkProps = {
@@ -8,7 +8,7 @@ type TagLinkProps = {
 };
 
 export const TagLink: React.FC<TagLinkProps> = ({ tag }) => {
-  const value = TagEnum[tag];
+  const value = valueOf(tag);
   const realPath = `/tag/${tag}`;
   const pathTemplate = "/tag/[id]";
   const path = isStatic(tag) ? realPath : pathTemplate;
