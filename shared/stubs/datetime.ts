07a27da937ef9d88b54aba8519f9fc0ee7a2d623
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-21 20:25:10 +0300

    Add date time stubs for convenient dates reading

---
+++ shared/stubs/datetime.ts
@@ -0,0 +1,4 @@
+import { DateTimeIsoString } from "@shared/types";
+
+export const defaultDatetime: DateTimeIsoString = "2020-08-04T12:00:00.000Z";
+export const oneDayAfter: DateTimeIsoString = "2020-08-05T12:00:00.000Z";
