22cf9e6eaed727bf75b243cb1b4de2a066af101f
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-23 16:03:33 +0300

    Extract the check for “collectionnes” of an argument in a util from the `sizeOf`

--- shared/sizeOf/index.ts
+++ shared/sizeOf/index.ts
@@ -1,12 +1,10 @@
 import { AnyCollection } from "shared/types";
+import { isCollection } from "@shared/isCollection";
 
 type CollectionSize = number;
 
 export function sizeOf<TCollection extends AnyCollection>(collection: TCollection): CollectionSize {
-  if (!collection || typeof collection !== "object") {
-    throw new Error("Collection must be a list or an object.");
-  }
-
+  if (!isCollection(collection)) throw new Error("Collection must be a list or an object.");
   if (typeof collection.length === "number") return collection.length;
   return Object.keys(collection).length;
 }
