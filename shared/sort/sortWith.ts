04ed09d56ee5b0c23d778010412d7df46e562e15
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-23 15:21:21 +0300

    Introduce `CompareResult` enum for clarification of sorting results

--- shared/sort/sortWith.ts
+++ shared/sort/sortWith.ts
@@ -5,16 +5,23 @@ export enum SortDirection {
   Descending = "desc",
 }
 
+export enum CompareResult {
+  AThenB = -1,
+  BThenA = 1,
+  Equal = 0,
+}
+
 type Sortable = AnyObject | AnyComparable;
-type CompareFunction = (a: Sortable, b: Sortable) => number;
+type CompareFunction = (a: Sortable, b: Sortable) => CompareResult;
 type TransformFunction = (arg: Sortable) => AnyComparable;
 
 export function sortWith(
   transform: TransformFunction,
   direction: SortDirection = SortDirection.Ascending,
 ): CompareFunction {
-  return function (a: Sortable, b: Sortable): number {
-    const compareResult = direction === SortDirection.Ascending ? -1 : 1;
-    return transform(a) < transform(b) ? compareResult : -compareResult;
+  return function (a: Sortable, b: Sortable): CompareResult {
+    const ascending = direction === SortDirection.Ascending;
+    const result = ascending ? CompareResult.AThenB : CompareResult.BThenA;
+    return transform(a) < transform(b) ? result : -result;
   };
 }
