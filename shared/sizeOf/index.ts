3674778ad77542a79f844f74d3739a76fa2b8db3
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-23 15:56:49 +0300

    Throw an error if a `sizeOf` receives not a collection as an argument

--- shared/sizeOf/index.ts
+++ shared/sizeOf/index.ts
@@ -3,6 +3,10 @@ import { AnyCollection } from "shared/types";
 type CollectionSize = number;
 
 export function sizeOf<TCollection extends AnyCollection>(collection: TCollection): CollectionSize {
+  if (!collection || typeof collection !== "object") {
+    throw new Error("Collection must be a list or an object.");
+  }
+
   if (typeof collection.length === "number") return collection.length;
   return Object.keys(collection).length;
 }
