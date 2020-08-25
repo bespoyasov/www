cf8a6fc589749df5b68716964f2c138710e0ed5a
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-25 22:15:47 +0300

    Cover the `projectsList` utility with tests

---
+++ persistence/source/names/index.test.ts
@@ -0,0 +1,23 @@
+import { assureType } from "@shared/assureType";
+import { SystemType } from "../composition";
+import { projectsList } from ".";
+
+function mockSystem(method: keyof SystemType, implementation: () => unknown): SystemType {
+  return assureType<SystemType>({ [method]: implementation });
+}
+
+describe("persistence > source > names > projectsList", () => {
+  it("should return a list of filenames without extensions in projects directory", () => {
+    const files = ["file1.mdx", "file2.mdx", "3.mdx"];
+    const expected = ["file1", "file2", "3"];
+    const system = mockSystem("readdirSync", () => files);
+    expect(projectsList({ system })).toEqual(expected);
+  });
+
+  it("should keep only .mdx files and filter out every other extension", () => {
+    const files = ["file1.mdx", "file2.mdx", "file3.md", "file4.tsx", "file5.ts"];
+    const expected = ["file1", "file2"];
+    const system = mockSystem("readdirSync", () => files);
+    expect(projectsList({ system })).toEqual(expected);
+  });
+});
