48c58bfda4295824e70bb45505cb2e430faa37f4
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-27 17:19:18 +0300

    Add a tests layout for the `exportsOf`

---
+++ domain/ast/exportsOf/exportsOf.test.ts
@@ -0,0 +1,10 @@
+import { exportsOf } from ".";
+
+describe("domain > AST > exportsOf", () => {
+  it.todo("should return a transformer function");
+  it.todo("should keep the root node in a given tree");
+  it.todo("should keep a named import node if allowed");
+  it.todo("should not keep a named import node if not allowed");
+  it.todo("should keep a default import node if allowed");
+  it.todo("should not keep a default import node if not allowed");
+});
