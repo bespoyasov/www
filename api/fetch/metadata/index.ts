fa696f13029ce1364332f7403321710d992892d5
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-28 20:05:02 +0300

    Extract factory into its own module to be able to export it for testing purposes, but not to expose it as a public API

--- api/fetch/metadata/index.ts
+++ api/fetch/metadata/index.ts
@@ -1,35 +1,5 @@
-import remark from "remark";
-import markdown from "remark-mdx";
-import { List } from "@shared/types";
-import { byDateDescending } from "@shared/sort";
-import { parseObject } from "@shared/parseObject";
-import { Metadata } from "@domain/metadata";
-import { exportsOf, metadataOf } from "@domain/ast";
 import { allProjects, allBlogPosts } from "@persistence/source";
-
-type Query = typeof allProjects | typeof allBlogPosts;
-type FetchRequest = () => Promise<List<Metadata>>;
-
-function metadataFor(query: Query): FetchRequest {
-  return async function request(): Promise<List<Metadata>> {
-    const posts = query();
-    const results: List<Metadata> = [];
-
-    for await (const post of posts) {
-      const result = await remark()
-        .use(markdown)
-        .use(exportsOf, { ignoreDefaults: true })
-        .use(metadataOf)
-        .process(post);
-
-      const object = parseObject<Metadata>(result.toString());
-      results.push(object);
-    }
-
-    results.sort(byDateDescending);
-    return results;
-  };
-}
+import { metadataFor } from "./factory";
 
 export const projectsMetadata = metadataFor(allProjects);
 export const blogPostsMetadata = metadataFor(allBlogPosts);
