048112f1d0e3048189b0110ae2981d2809cc2541
Alexander Bespoyasov <forjob159@gmail.com>
2020-08-29 14:54:43 +0300

    Cover the `metadataFor` with tests

---
+++ api/fetch/metadata/factory.test.ts
@@ -0,0 +1,44 @@
+import markdown from "remark-mdx";
+import { exportsOf, metadataOf } from "@domain/ast";
+import { FileContentProcessorMock } from "@shared/mocks";
+import { metadataFor } from "./factory";
+
+const mockInstance = new FileContentProcessorMock(`{datetime: "2020-08-28T12:00:00"}`);
+jest.mock("remark", () => () => mockInstance);
+
+describe("api > fetch > metadata > metadataFor", () => {
+  it("should call a given query when request fired", () => {
+    const query = jest.fn(() => []);
+    const request = metadataFor(query);
+
+    request();
+    expect(query).toHaveBeenCalled();
+  });
+
+  it("should apply all the `remark` plugins on each post in query result", async () => {
+    const posts = ["post1", "post2"];
+    const plugins = [markdown, exportsOf, metadataOf];
+
+    const query = () => posts;
+    const request = metadataFor(query);
+    await request();
+
+    const mockCalledTimes = posts.length * plugins.length;
+    expect(mockInstance.inspect).toHaveBeenCalledTimes(mockCalledTimes);
+
+    plugins.forEach((plugin, index) => {
+      expect(mockInstance.inspect.mock.calls[index][0]).toEqual(plugin);
+    });
+  });
+
+  it("should sort query results with a given compare function", async () => {
+    const posts = ["post1", "post2", "post3"];
+    const query = () => posts;
+    const sorter = jest.fn();
+
+    const request = metadataFor(query, { sorter });
+    await request();
+
+    expect(sorter).toHaveBeenCalledTimes(posts.length - 1);
+  });
+});
