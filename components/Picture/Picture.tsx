c04c3c685b9d04ba74f14c15f1b08732ba860a6b
Alexander Bespoyasov <forjob159@gmail.com>
2020-09-04 11:46:49 +0300

    Render a `source` with `webp`-image in a `Picture` component

--- components/Picture/Picture.tsx
+++ components/Picture/Picture.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { classes } from "@shared/classes";
 import { PictureProps } from "./types";
 import { isLimited } from "./isLimited";
+import { withWebp } from "./withWebp";
 import styles from "./Picture.module.css";
 
 const { picture, limited } = styles;
@@ -11,6 +12,7 @@ export const Picture: React.FC<PictureProps> = ({ src, alt, cite, caption }) => 
     <figure className={classes(picture, isLimited(src) && limited)}>
       <a href={cite}>
         <picture>
+          <source srcSet={withWebp(src)} type="image/webp" />
           <img src={src} alt={alt} loading="lazy" />
         </picture>
       </a>
