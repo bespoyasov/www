4f6721afffcc1ae13da08f97307205336431595a
Alexander Bespoyasov <forjob159@gmail.com>
2020-09-06 14:14:36 +0300

    Rename a link to an editorial tag page

--- components/Nav/routes.ts
+++ components/Nav/routes.ts
@@ -4,7 +4,7 @@ import { NavRoute, RouteKind } from "./types";
 export const internal: List<NavRoute<RouteKind.Internal>> = [
   { url: "/projects", title: "Проекты" },
   { url: "/blog", title: "Блог" },
-  { url: "/tag/editorial", title: "Книги" },
+  { url: "/tag/editorial", title: "Лучшее" },
 ];
 
 export const external: List<NavRoute<RouteKind.External>> = [
