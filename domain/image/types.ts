124472a4dbb825077dff9028907446c5654abc7d
Alexander Bespoyasov <forjob159@gmail.com>
2020-09-15 21:23:19 +0300

    Introduce an `ImageDescription` type alias for alt-text strings and captions

--- domain/image/types.ts
+++ domain/image/types.ts
@@ -2,5 +2,6 @@ import { RelativePath } from "@shared/types";
 
 export type ImageSource = RelativePath;
 export type ImageExtension = typeof imageExtensions[number];
+export type ImageDescription = string;
 
 export const imageExtensions = ["png", "jpg", "webp"] as const;
