483b464376570a1cc37d2ae9c9848a7d8f58c35e
Alexander Bespoyasov <forjob159@gmail.com>
2020-09-15 21:46:10 +0300

    Reduce duplication by replacing a similar type with already existing one from the `@domain`

--- components/Figure/isLimited.ts
+++ components/Figure/isLimited.ts
@@ -1,9 +1,9 @@
-import { ImageFileSource } from "./types";
+import { ImageSource } from "@domain/image";
 
 type ShouldLimitContainerWidth = boolean;
 
 const LIMITED_IMAGE_POSTFIX = "?limited";
 
-export function isLimited(src: ImageFileSource): ShouldLimitContainerWidth {
+export function isLimited(src: ImageSource): ShouldLimitContainerWidth {
   return src.endsWith(LIMITED_IMAGE_POSTFIX);
 }
