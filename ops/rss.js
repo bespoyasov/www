7b4cf83d7ebd1cadd34454be9e5fa735d26306d3
Alexander Bespoyasov <forjob159@gmail.com>
2020-11-28 22:12:18 +0300

    Use `feed` package to generate an RSS feed

--- ops/rss.js
+++ ops/rss.js
@@ -2,39 +2,8 @@
 const { readFile, writeFile } = require("fs").promises;
 const path = require("path");
 const jsdom = require("jsdom");
+const { Feed } = require("feed");
 const { JSDOM } = jsdom;
-
-function toRFC822(date) {
-  return new Date(date).toUTCString();
-}
-
-function fromTemplate({ title, description, lastBuildDate, channel }) {
-  return `<?xml version="1.0" encoding="UTF-8" ?>
-  <rss version="2.0">
-    <channel>
-      <title>${title}</title>
-      <link>https://bespoyasov.ru</link>
-      <description>${description}</description>
-      <language>ru-ru</language>
-      <lastBuildDate>${lastBuildDate}</lastBuildDate>
-      <copyright>Саша Беспоясов</copyright>
-
-      ${channel}
-    </channel>
-  </rss>`;
-}
-
-function fromPostTemplate({ title, link, datetime, description }) {
-  return `<item>
-    <title>${title}</title>
-    <link>${link}</link>
-    <pubDate>${datetime}</pubDate>
-    <guid>${link}</guid>
-    <description>
-      <![CDATA[ ${description} ]]>
-    </description>
-  </item>`;
-}
 
 const WORKING_DIR = process.cwd();
 const SOURCE_PATH = path.join(WORKING_DIR, "out/rss/index.html");
@@ -51,24 +20,42 @@ async function saveAsFile(contents) {
 
 async function generateRss() {
   const feedHtml = await fromFile();
-  const feed = feedHtml.window.document;
-  const title = feed.querySelector("title").textContent;
-  const description = feed.querySelector(`[name="description"]`).getAttribute("content");
+  const feedRoot = feedHtml.window.document;
 
-  const posts = feed.querySelectorAll("article");
-  const lastBuildDate = toRFC822(posts[0].dataset.datetime);
+  const title = feedRoot.querySelector("title").textContent;
+  const description = feedRoot.querySelector(`[name="description"]`).getAttribute("content");
 
-  let channel = "";
+  const posts = feedRoot.querySelectorAll("article");
+  const updated = new Date(posts[0].dataset.datetime);
+
+  const name = "Саша Беспоясов";
+  const link = "https://bespoyasov.ru";
+
+  const feed = new Feed({
+    id: link,
+    link,
+    title,
+    description,
+    updated,
+    author: { name, link },
+    language: "ru",
+  });
 
   for (const post of posts) {
     const { title, link, datetime: datetimeISO } = post.dataset;
-    const datetime = toRFC822(datetimeISO);
-    const description = post.innerHTML;
-    channel += fromPostTemplate({ title, link, datetime, description });
+    const date = new Date(datetimeISO);
+    const content = post.innerHTML;
+
+    feed.addItem({
+      id: link,
+      link,
+      title,
+      content,
+      date,
+    });
   }
 
-  const rssXml = fromTemplate({ title, description, lastBuildDate, channel });
-  await saveAsFile(rssXml);
+  await saveAsFile(feed.rss2());
 }
 
 (async () => {
