7ad5411a026fbe29de94f768176509bb8917d172
Alexander Bespoyasov <forjob159@gmail.com>
2020-12-24 21:01:45 +0300

    Drop obsolete favicons support

--- components/HeadContents/HtmlHead.tsx
+++ components/HeadContents/HtmlHead.tsx
@@ -8,14 +8,12 @@ export const HeadContents: React.FC = () => {
       <link rel="author" href="/humans.txt" />
       <link rel="alternate" type="application/rss+xml" title="RSS" href="/rss.xml" />
 
-      <link rel="apple-touch-icon" sizes="180x180" href="/meta/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/meta/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/meta/favicon-16x16.png" />
+      <link rel="icon" href="/meta/favicon.ico" />
+      <link rel="icon" href="/meta/icon.svg" type="image/svg+xml" sizes="any" />
+      <link rel="apple-touch-icon" href="/meta/favicon-180x180.png" />
       <link rel="manifest" href="/meta/site.webmanifest" />
-      <link rel="mask-icon" href="/meta/safari-pinned-tab.svg" color="#333333" />
 
       <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="msapplication-TileImage" content="/meta/mstile-144x144.png" />
       <meta name="msapplication-config" content="/meta/browserconfig.xml" />
       <meta name="theme-color" content="#ffffff" />
 
