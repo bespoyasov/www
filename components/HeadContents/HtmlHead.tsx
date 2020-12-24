9d6ab3b4e5a69c25b5ad23ae025effc0e1bef801
Alexander Bespoyasov <forjob159@gmail.com>
2020-12-24 21:06:35 +0300

    Move `favicon.ico` to the site root

--- components/HeadContents/HtmlHead.tsx
+++ components/HeadContents/HtmlHead.tsx
@@ -8,7 +8,7 @@ export const HeadContents: React.FC = () => {
       <link rel="author" href="/humans.txt" />
       <link rel="alternate" type="application/rss+xml" title="RSS" href="/rss.xml" />
 
-      <link rel="icon" href="/meta/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" />
       <link rel="icon" href="/meta/icon.svg" type="image/svg+xml" sizes="any" />
       <link rel="apple-touch-icon" href="/meta/favicon-180x180.png" />
       <link rel="manifest" href="/meta/site.webmanifest" />
