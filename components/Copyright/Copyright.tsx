a9ad444132183a1391ff6bf87266163b2302160d
Alexander Bespoyasov <forjob159@gmail.com>
2021-01-09 00:17:54 +0300

    Remove holiday decorations from `Footer`

--- components/Copyright/Copyright.tsx
+++ components/Copyright/Copyright.tsx
@@ -4,7 +4,7 @@ import styles from "./Copyright.module.css";
 export const Copyright: React.FC = () => {
   return (
     <div>
-      <span className={styles.dates}>☃️ 2010+</span>
+      <span className={styles.dates}>2010+</span>
       <a href="mailto:bespoyasov@me.com" className="text-color">
         bespoyasov@me.com
       </a>
