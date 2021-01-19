f23c1cd4fb80879ea1610e998d300361bd283a76
Alexander Bespoyasov <forjob159@gmail.com>
2021-01-19 13:22:57 +0300

    Remove obsolete shared types imports missed before

--- shared/isEmpty/index.ts
+++ shared/isEmpty/index.ts
@@ -1,4 +1,3 @@
-import { AnyCollection } from "shared/types";
 import { sizeOf } from "@shared/sizeOf";
 
 export function isEmpty<TCollection extends AnyCollection>(collection: TCollection): boolean {
