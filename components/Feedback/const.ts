6f13a9dc74307edcb6dfa0c362c6d6ba7cca50cd
Alexander Bespoyasov <forjob159@gmail.com>
2021-04-16 18:07:06 +0300

    Add social media share links templates

---
+++ components/Feedback/const.ts
@@ -0,0 +1,3 @@
+export const FACEBOOK_TEMPLATE = "https://facebook.com/sharer.php?u={URL}";
+export const TWITTER_TEMPLATE =
+  "https://twitter.com/intent/tweet?text={TITLE}&url={URL}&via=bespoyasov";
