d06c0f551b84ff9a1d65e01c63b84e5aa24b14f3
Alexander Bespoyasov <forjob159@gmail.com>
2021-04-16 18:07:51 +0300

    Create share links factories

---
+++ components/Feedback/shareLinkFactory.test.ts
@@ -0,0 +1,25 @@
+import { metadata } from "@shared/stubs";
+import { createLinkFactory } from "./shareLinkFactory";
+
+const testTemplate = "https://example.site/?url={URL}&text={TITLE}";
+const testSlug = "/test/page";
+
+describe("when called a factory creator", () => {
+  it("should return a function", () => {
+    const createLink = createLinkFactory(testTemplate);
+    expect(typeof createLink).toEqual("function");
+  });
+});
+
+describe("when given post metadata to a link factory", () => {
+  const createLink = createLinkFactory(testTemplate);
+  const result = createLink({ ...metadata, slug: testSlug });
+
+  it("should replace the {URL} slot with a given slug's absolute URL", () => {
+    expect(result).toContain(`?url=https://bespoyasov.ru${testSlug}`);
+  });
+
+  it("should replace the {TITLE} slot with a post title", () => {
+    expect(result).toContain(`&text=${metadata.title}`);
+  });
+});
---
+++ components/Feedback/shareLinkFactory.ts
@@ -0,0 +1,15 @@
+import { Metadata } from "@domain/metadata";
+import { absoluteUrlFor } from "@shared/absoluteUrl";
+import { FACEBOOK_TEMPLATE, TWITTER_TEMPLATE } from "./const";
+
+type LinkTemplate = AbsoluteUrl;
+
+export function createLinkFactory(template: LinkTemplate) {
+  return function createLink({ slug, title }: Metadata): AbsoluteUrl {
+    const url = absoluteUrlFor(slug);
+    return template.replace("{URL}", url).replace("{TITLE}", title);
+  };
+}
+
+export const twitterLink = createLinkFactory(TWITTER_TEMPLATE);
+export const facebookLink = createLinkFactory(FACEBOOK_TEMPLATE);
