356d9965b638835f5b27dd18a8000ef01fa535bb
Alexander Bespoyasov <forjob159@gmail.com>
2021-05-07 15:02:29 +0200

    Extract `QueryKind` into the types

--- persistence/source/contents/one/index.ts
+++ persistence/source/contents/one/index.ts
@@ -1,8 +1,9 @@
 import path from "path";
 import { PostContents, PostId } from "@domain/post";
 import { withMdx } from "@persistence/utils";
+import { QueryKind } from "@persistence/types";
+import { Dependencies, di } from "@persistence/composition";
 import { PROJECTS_DIRECTORY, BLOG_DIRECTORY } from "@persistence/const";
-import { Dependencies, di, QueryKind } from "@persistence/composition";
 
 type Executor = (id: PostId, di?: Dependencies) => PostContents;
 
