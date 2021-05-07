543ab1ed1808f803fdfdfcd3f96f35c523b6bd59
Alexander Bespoyasov <forjob159@gmail.com>
2021-05-07 21:38:14 +0200

    Remove MDX serialization configs from Next config

--- next.config.js
+++ next.config.js
@@ -1,36 +1,7 @@
 /* eslint-disable no-undef, @typescript-eslint/no-var-requires */
 const withOffline = require("next-offline");
-const withMDX = require("@next/mdx")({
-  options: {
-    remarkPlugins: [
-      require("remark-unwrap-images"),
-      [
-        require("@mavrin/remark-typograf"),
-        {
-          locale: ["ru"],
-          disableRule: ["*"],
-          enableRule: ["common/nbsp/*", "ru/nbsp/*"],
-        },
-      ],
-    ],
 
-    rehypePlugins: [
-      [
-        require("rehype-highlight/light"),
-        {
-          languages: {
-            javascript: require("highlight.js/lib/languages/javascript"),
-            typescript: require("highlight.js/lib/languages/typescript"),
-          },
-        },
-      ],
-    ],
-  },
+module.exports = withOffline({
+  pageExtensions: ["tsx"],
+  trailingSlash: true,
 });
-
-module.exports = withOffline(
-  withMDX({
-    pageExtensions: ["tsx"],
-    trailingSlash: true,
-  }),
-);
