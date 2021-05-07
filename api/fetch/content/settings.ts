c59078e6a5b63c6a55bc40317bf78c962d21ce7b
Alexander Bespoyasov <forjob159@gmail.com>
2021-05-07 21:37:49 +0200

    Extract MDX serialization configs into `@configs`

--- api/fetch/content/settings.ts
+++ api/fetch/content/settings.ts
@@ -1,36 +1,7 @@
 import { SerializeOptions } from "next-mdx-remote/dist/types";
-
-import * as unwrapImages from "remark-unwrap-images";
-import * as typography from "@mavrin/remark-typograf";
-
-import * as syntaxHighlight from "rehype-highlight/light";
-import * as javascript from "highlight.js/lib/languages/javascript";
-import * as typescript from "highlight.js/lib/languages/typescript";
+import { serializationConfig } from "@configs/serialization";
+import { assureType } from "@shared/assureType";
 
 export const settings: SerializeOptions = {
-  mdxOptions: {
-    remarkPlugins: [
-      unwrapImages,
-      [
-        typography,
-        {
-          locale: ["ru"],
-          disableRule: ["*"],
-          enableRule: ["common/nbsp/*", "ru/nbsp/*"],
-        },
-      ],
-    ],
-
-    rehypePlugins: [
-      [
-        syntaxHighlight,
-        {
-          languages: {
-            javascript,
-            typescript,
-          },
-        },
-      ],
-    ],
-  },
+  mdxOptions: assureType(serializationConfig),
 };
---
+++ configs/serialization.ts
@@ -0,0 +1,32 @@
+import * as unwrapImages from "remark-unwrap-images";
+import * as typography from "@mavrin/remark-typograf";
+
+import * as syntaxHighlight from "rehype-highlight/light";
+import * as javascript from "highlight.js/lib/languages/javascript";
+import * as typescript from "highlight.js/lib/languages/typescript";
+
+export const serializationConfig = {
+  remarkPlugins: [
+    unwrapImages,
+    [
+      typography,
+      {
+        locale: ["ru"],
+        disableRule: ["*"],
+        enableRule: ["common/nbsp/*", "ru/nbsp/*"],
+      },
+    ],
+  ],
+
+  rehypePlugins: [
+    [
+      syntaxHighlight,
+      {
+        languages: {
+          javascript,
+          typescript,
+        },
+      },
+    ],
+  ],
+};
