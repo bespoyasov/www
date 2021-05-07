826ff88117b5649aa384715b294890bb28070c94
Alexander Bespoyasov <forjob159@gmail.com>
2021-05-07 20:54:38 +0200

    Load posts contents via fetch API in blog and projects

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,32 +1,36 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
 import { GetStaticProps, GetStaticPaths } from "next";
 
+import { PostContents } from "@domain/post";
 import { Metadata as MetadataType } from "@domain/metadata";
-import { blogPostsMetadata, blogPostsNames } from "@api/fetch";
+import { blogPostsMetadata, blogPostsNames, fetchBlogPost } from "@api/fetch";
 
-import { PostLayout } from "@layouts/Post";
+import { Post } from "@components/Post";
 import { Adjacent } from "@components/Adjacent";
 import { Metadata } from "@components/Metadata";
 import { Feedback } from "@components/Feedback";
 import { Description } from "@components/Description";
 
 type BlogPostProps = {
+  contents: PostContents;
   metadata: MetadataType;
   prevPost: Nullable<MetadataType>;
   nextPost: Nullable<MetadataType>;
 };
 
 export const getStaticProps: GetStaticProps<BlogPostProps> = async (context) => {
-  const { id } = context.params;
+  const postId = String(context.params.id);
+
+  const content = await fetchBlogPost(postId);
   const blogPosts = await blogPostsMetadata();
-  const index = blogPosts.findIndex((post) => post.slug.endsWith(String(id)));
+  const index = blogPosts.findIndex((post) => post.slug.endsWith(postId));
 
   return {
     props: {
       metadata: blogPosts[index],
       prevPost: blogPosts[index - 1] ?? null,
       nextPost: blogPosts[index + 1] ?? null,
+      contents: content,
     },
   };
 };
@@ -37,25 +41,19 @@ export const getStaticPaths: GetStaticPaths = async () => {
   return { paths, fallback: false };
 };
 
-const BlogPost = ({ metadata, prevPost, nextPost }: BlogPostProps) => {
-  const { title, description } = metadata;
-  const { query } = useRouter();
-  const PostContents = require(`./${query.id}.mdx`).default;
-
+const BlogPost = ({ metadata, prevPost, nextPost, contents }: BlogPostProps) => {
   return (
-    <PostLayout>
+    <>
       <Head>
-        <title>{title}</title>
-        <Description>{description}</Description>
+        <title>{metadata.title}</title>
+        <Description>{metadata.description}</Description>
       </Head>
-      <main>
-        <PostContents />
-      </main>
 
+      <Post content={contents} />
       <Metadata metadata={metadata} />
       <Feedback metadata={metadata} />
       <Adjacent prev={prevPost} next={nextPost} />
-    </PostLayout>
+    </>
   );
 };
 
