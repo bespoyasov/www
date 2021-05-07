f115998622bc2e466e7f717976f3eeeb3abe5e6f
Alexander Bespoyasov <forjob159@gmail.com>
2021-05-07 09:36:16 +0200

    Extract `QueryKind` into composition since we're gonna need it later

--- persistence/source/composition.ts
+++ persistence/source/composition.ts
@@ -1,5 +1,8 @@
 import fs from "fs";
 import { SystemType } from "@persistence/types";
+import { BLOG_DIRECTORY, PROJECTS_DIRECTORY } from "@persistence/const";
+
+export type QueryKind = typeof PROJECTS_DIRECTORY | typeof BLOG_DIRECTORY;
 
 export type Dependencies = {
   system?: SystemType;
