de4ec5da7a96cad602cb72d0c675f45407f383b4
Alexander Bespoyasov <forjob159@gmail.com>
2021-05-26 13:44:33 +0300

    Use an absolute url for social media preview images

--- components/SummaryCard/const.ts
+++ components/SummaryCard/const.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_TITLE = "АБ";
 export const DEFAULT_DESCRIPTION = "Блог о разработке.";
 
-export const PREVIEW_IMAGE = "/img/common/social-media-preview.png";
+export const PREVIEW_IMAGE = "https://bespoyasov.ru/img/common/social-media-preview.png";
