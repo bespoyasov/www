95713f45d4bff67f61e13118aa600b752266f44c
Alexander Bespoyasov <forjob159@gmail.com>
2021-05-26 13:35:29 +0300

    Add a static social media preview image

--- components/SummaryCard/SummaryCard.tsx
+++ components/SummaryCard/SummaryCard.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "@domain/metadata";
-import { DEFAULT_TITLE, DEFAULT_DESCRIPTION } from "./const";
+import { DEFAULT_TITLE, DEFAULT_DESCRIPTION, PREVIEW_IMAGE } from "./const";
 
 type SummaryCardProps = {
   metadata?: Metadata;
@@ -14,10 +14,12 @@ export const SummaryCard = ({ metadata }: SummaryCardProps) => {
       <meta name="twitter:site" content="@bespoyasov" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={PREVIEW_IMAGE} />
 
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+      <meta property="og:image" content={PREVIEW_IMAGE} />
     </>
   );
 };
--- components/SummaryCard/const.ts
+++ components/SummaryCard/const.ts
@@ -1,2 +1,4 @@
 export const DEFAULT_TITLE = "АБ";
 export const DEFAULT_DESCRIPTION = "Блог о разработке.";
+
+export const PREVIEW_IMAGE = "/img/common/social-media-preview.png";
