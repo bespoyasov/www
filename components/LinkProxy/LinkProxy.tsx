885ae9531af9bfe9f650ba93348fde7e67a46e68
Alexander Bespoyasov <forjob159@gmail.com>
2021-05-26 20:54:24 +0300

    Switch from using list of external protocols to allow-list of internal protocol for `LinkProxy`

--- components/LinkProxy/LinkProxy.tsx
+++ components/LinkProxy/LinkProxy.tsx
@@ -1,18 +1,18 @@
 import { AnchorHTMLAttributes } from "react";
 import Link from "next/link";
 
-function isExternalRoute(uri: AbsoluteUrl | UrlSlug): boolean {
-  return uri.startsWith("http") || uri.startsWith("mailto");
+function isInternalRoute(uri: AbsoluteUrl | UrlSlug): boolean {
+  return uri.startsWith("/");
 }
 
 export const LinkProxy = (props: AnchorHTMLAttributes<HTMLAnchorElement>) => {
   const { href, ...rest } = props;
 
-  return isExternalRoute(href) ? (
-    <a {...props} />
-  ) : (
+  return isInternalRoute(href) ? (
     <Link href={href}>
       <a {...rest} />
     </Link>
+  ) : (
+    <a {...props} />
   );
 };
