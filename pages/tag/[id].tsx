87104223f012a72e22b9d4c02a3d5075578046c8
Alexander Bespoyasov <forjob159@gmail.com>
2021-06-01 19:23:25 +0300

    Group posts by year on a tag page

--- pages/tag/[id].tsx
+++ pages/tag/[id].tsx
@@ -6,7 +6,7 @@ import { VisuallyHidden } from "@components/VisuallyHidden";
 import { Description } from "@components/Description";
 import { ProjectsList } from "@components/ProjectsList";
 import { Section } from "@components/Section";
-import { Group } from "@components/Notes";
+import { Anthology } from "@components/Notes";
 
 import { assureType } from "@shared/assureType";
 import { sizeOf } from "@shared/sizeOf";
@@ -64,7 +64,7 @@ const Tag = ({ projects, notes }: TagProps) => {
         <VisuallyHidden as="h1">{summary}</VisuallyHidden>
 
         <Section title={`Заметки по тегу «${tag}»`}>
-          <Group notes={notes} />
+          <Anthology notes={notes} />
         </Section>
 
         {sizeOf(projects) > 0 && (
