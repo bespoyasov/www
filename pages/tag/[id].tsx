1e493336c682bf7b45c3e6477e41860039288c13
Alexander Bespoyasov <forjob159@gmail.com>
2021-06-01 19:21:28 +0300

    Replace the old `All` component with `Anthology`

--- pages/tag/[id].tsx
+++ pages/tag/[id].tsx
@@ -5,8 +5,8 @@ import { GetStaticProps, GetStaticPaths } from "next";
 import { VisuallyHidden } from "@components/VisuallyHidden";
 import { Description } from "@components/Description";
 import { ProjectsList } from "@components/ProjectsList";
-import { AllNotes } from "@components/Notes";
 import { Section } from "@components/Section";
+import { Group } from "@components/Notes";
 
 import { assureType } from "@shared/assureType";
 import { sizeOf } from "@shared/sizeOf";
@@ -64,7 +64,7 @@ const Tag = ({ projects, notes }: TagProps) => {
         <VisuallyHidden as="h1">{summary}</VisuallyHidden>
 
         <Section title={`Заметки по тегу «${tag}»`}>
-          <AllNotes notes={notes} />
+          <Group notes={notes} />
         </Section>
 
         {sizeOf(projects) > 0 && (
