36d554cf9aaa95aab487e8eaa6b02d3d378d772d
Alexander Bespoyasov <forjob159@gmail.com>
2021-06-01 19:09:59 +0300

    Add a stub for the date a year before the default datetime

--- shared/stubs/datetime.ts
+++ shared/stubs/datetime.ts
@@ -1,2 +1,3 @@
 export const defaultDatetime: DateTimeIsoString = "2020-08-04T12:00:00.000Z";
 export const oneDayAfter: DateTimeIsoString = "2020-08-05T12:00:00.000Z";
+export const yearBefore: DateTimeIsoString = "2019-08-04T12:00:00.000Z";
