d9f36aa39eab6bc8bb8f1bb355108157a4d9251e
Alexander Bespoyasov <forjob159@gmail.com>
2021-08-16 15:02:17 +0300

    Rewrite image optimizator with Squoosh

--- ops/graphics.js
+++ ops/graphics.js
@@ -1,22 +1,32 @@
-/* eslint-disable no-undef, @typescript-eslint/no-var-requires */
 const { promises, existsSync: exists } = require("fs");
 const { resolve, join } = require("path");
-const { readdir } = promises;
+const { ImagePool } = require("@squoosh/lib");
 
-const imagemin = require("imagemin");
-const imageminWebp = require("imagemin-webp");
+const { readdir, writeFile } = promises;
+const imagePool = new ImagePool();
 
-const EXTENSIONS = [".jpg", ".png"];
+const SOURCE_EXTENSIONS = ["jpg", "png"];
+const TARGET_EXTENSIONS = ["webp"];
+const ENCODE_OPTIONS = {
+  webp: {},
+};
+
 const ROOT_DIRECTORY = process.cwd();
 const IMAGES_DIRECTORY = join(ROOT_DIRECTORY, "public/img");
 
-function withWebp(source) {
-  const imageExtension = new RegExp(EXTENSIONS.join("|"), "i");
-  return source.replace(imageExtension, ".webp");
+function withExtension(extension, filename) {
+  const base = filename.slice(0, filename.lastIndexOf("."));
+  return `${base}.${extension}`;
 }
 
-function directoryOf(filename) {
-  return filename.slice(0, filename.lastIndexOf("/"));
+function isImage(filename) {
+  return SOURCE_EXTENSIONS.some((e) => filename.includes(`.${e}`));
+}
+
+function hasOptimizedVersions(filename) {
+  return TARGET_EXTENSIONS.map((extension) => withExtension(extension, filename))
+    .map((filename) => exists(filename))
+    .reduce((result, exists) => result && exists, true);
 }
 
 async function filesIn(directory) {
@@ -34,21 +44,28 @@ async function filesIn(directory) {
 async function imagesToConvert() {
   const files = await filesIn(IMAGES_DIRECTORY);
   return files.filter((filename) => {
-    const isImage = EXTENSIONS.some((e) => filename.includes(e));
-    const hasWebp = exists(withWebp(filename));
-    return isImage && !hasWebp;
+    return isImage(filename) && !hasOptimizedVersions(filename);
   });
 }
 
-async function convert(image) {
-  await imagemin([image], {
-    destination: directoryOf(image),
-    plugins: [imageminWebp()],
-  });
+async function saveConverted(image, filename) {
+  for (const extension of TARGET_EXTENSIONS) {
+    const { binary } = await image.encodedWith[extension];
+    const converted = withExtension(extension, filename);
+    await writeFile(converted, binary);
+  }
+}
+
+async function convert(filename) {
+  const image = imagePool.ingestImage(filename);
+  await image.encode(ENCODE_OPTIONS);
+  await saveConverted(image, filename);
 }
 
 (async () => {
   for (const image of await imagesToConvert()) {
     await convert(image);
   }
+
+  await imagePool.close();
 })();
