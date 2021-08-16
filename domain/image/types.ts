7a988653cbd6561d7f2d6c2f7f2dc0eb5b1b1338
Alexander Bespoyasov <forjob159@gmail.com>
2021-08-16 15:27:43 +0300

    Include AVIF in the extensions list

--- domain/image/types.ts
+++ domain/image/types.ts
@@ -2,4 +2,4 @@ export type ImageDescription = string;
 export type ImageFileSource = RelativePath;
 export type ImageExtension = typeof imageExtensions[number];
 
-export const imageExtensions = ["png", "jpg", "webp"] as const;
+export const imageExtensions = ["png", "jpg", "webp", "avif"] as const;
