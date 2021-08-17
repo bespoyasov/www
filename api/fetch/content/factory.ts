8e1c08287f12316d180d3c0815a1e7fb7eafe434
Alexander Bespoyasov <forjob159@gmail.com>
2021-08-17 23:05:19 +0300

    Implement content fetch using `gray-matter`

--- api/fetch/content/factory.ts
+++ api/fetch/content/factory.ts
@@ -9,10 +9,10 @@ type FetchRequest = (id: PostId, di?: Dependencies) => Promise<PostContents>;
 export function contentFor(query: Query): FetchRequest {
   return async function request(
     id: PostId,
-    { serialize }: Dependencies = di,
+    { serialize, parser }: Dependencies = di,
   ): Promise<PostContents> {
-    const source = query(id);
-    const { compiledSource } = await serialize(source, settings);
+    const { content } = parser(query(id));
+    const { compiledSource } = await serialize(content, settings);
     return compiledSource;
   };
 }
