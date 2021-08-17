0ad9ce80da6fcd0820fefefdf390418f5dc37790
Alexander Bespoyasov <forjob159@gmail.com>
2021-08-17 23:05:00 +0300

    Introduce a new content parser

--- api/fetch/content/composition.ts
+++ api/fetch/content/composition.ts
@@ -1,10 +1,13 @@
+import parser from "gray-matter";
 import { serialize } from "next-mdx-remote/serialize";
-import { Serializer } from "./types";
+import { Serializer, Parser } from "../types";
 
 export type Dependencies = {
   serialize: Serializer;
+  parser: Parser;
 };
 
 export const di: Dependencies = {
   serialize,
+  parser,
 };
--- api/fetch/content/index.ts
+++ api/fetch/content/index.ts
@@ -1,6 +1,5 @@
 import { getBlogPost, getProject } from "@persistence/source";
 import { contentFor } from "./factory";
 
-export * from "./types";
 export const fetchProject = contentFor(getProject);
 export const fetchBlogPost = contentFor(getBlogPost);
