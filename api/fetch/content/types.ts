5adedb8663667480e3e243aa3bd37d119a43ae59
Alexander Bespoyasov <forjob159@gmail.com>
2021-08-17 23:03:39 +0300

    Extend content fetcher composition with `Parser` type

--- api/fetch/content/types.ts
+++ api/fetch/content/types.ts
@@ -1,5 +1,6 @@
-import { MDXRemoteSerializeResult } from "next-mdx-remote";
-import { SerializeOptions } from "next-mdx-remote/dist/types";
+import type { GrayMatterFile } from "gray-matter";
+import type { MDXRemoteSerializeResult } from "next-mdx-remote";
+import type { SerializeOptions } from "next-mdx-remote/dist/types";
 
 type SerializeSource = string;
 type SerializeResult = MDXRemoteSerializeResult;
@@ -8,3 +9,8 @@ export type Serializer = (
   source: SerializeSource,
   options?: SerializeOptions,
 ) => Promise<SerializeResult>;
+
+type ParseSource = string;
+type ParseResult = GrayMatterFile<string>;
+
+export type Parser = (source: ParseSource) => ParseResult;
