0fa831cf70cbb223ca22f3c3ac5e342b8368e91c
Alexander Bespoyasov <forjob159@gmail.com>
2021-09-11 18:12:35 +0300

    Update blog and projects directories

--- src/persistence/const.ts
+++ src/persistence/const.ts
@@ -1,5 +1,5 @@
 import path from "path";
 
 export const ROOT_DIRECTORY = process.cwd();
-export const BLOG_DIRECTORY = path.join(ROOT_DIRECTORY, "pages/blog");
-export const PROJECTS_DIRECTORY = path.join(ROOT_DIRECTORY, "pages/projects");
+export const BLOG_DIRECTORY = path.join(ROOT_DIRECTORY, "src/pages/blog");
+export const PROJECTS_DIRECTORY = path.join(ROOT_DIRECTORY, "src/pages/projects");
