e6c75ec007fda6f745424d295dab4a0fa7ce88fa
Alexander Bespoyasov <forjob159@gmail.com>
2021-12-01 20:42:20 +0100

    Update personality microdata

--- src/components/MicroData/Person.tsx
+++ src/components/MicroData/Person.tsx
@@ -4,7 +4,7 @@ export const Person = () => {
   return (
     <VisuallyHidden itemScope itemType="//schema.org/Person">
       <span itemProp="name">Саша Беспоясов</span>
-      <span itemProp="jobTitle">Фронтенд-разработчик</span>
+      <span itemProp="jobTitle">Разработчик, ментор, спикер</span>
       <span itemProp="email">bespoyasov@me.com</span>
     </VisuallyHidden>
   );
