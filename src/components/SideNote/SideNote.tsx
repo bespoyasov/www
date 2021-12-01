76fea7b49f28311f21532c87a5627213eaefb0ed
Alexander Bespoyasov <forjob159@gmail.com>
2021-12-01 09:56:20 +0100

    Shorten `className` for the `SideNote` component

--- src/components/SideNote/SideNote.tsx
+++ src/components/SideNote/SideNote.tsx
@@ -2,5 +2,5 @@ import { WithChildren } from "@extensions/components";
 import styles from "./SideNote.module.css";
 
 export const SideNote = ({ children }: WithChildren) => {
-  return <aside className={styles.sideNote}>{children}</aside>;
+  return <aside className={styles.aside}>{children}</aside>;
 };
