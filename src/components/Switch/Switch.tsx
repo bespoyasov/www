b6302e02b743395255b9d132592ec4d3db850e95
Alexander Bespoyasov <forjob159@gmail.com>
2021-12-02 18:08:22 +0100

    Create a `Switch` component for changing the preferred code samples programming language

---
+++ src/components/Switch/Switch.tsx
@@ -0,0 +1,23 @@
+import type { CodeSamplesLanguage } from "@domain/preferences";
+import type { WithChildren } from "@extensions/components";
+
+import { sizeOf } from "@shared/sizeOf";
+import { Samples } from "./Samples";
+import { Controls } from "./Controls";
+import styles from "./Switch.module.css";
+
+type SwitchProps = {
+  options: List<CodeSamplesLanguage>;
+};
+
+export const Switch = ({ options, children }: WithChildren<SwitchProps>) => {
+  if (!Array.isArray(children)) return <>{children}</>;
+  if (sizeOf(children) !== sizeOf(options)) return <>{children}</>;
+
+  return (
+    <figure className={styles.switch}>
+      <Controls options={options} />
+      <Samples options={options} samples={children} />
+    </figure>
+  );
+};
