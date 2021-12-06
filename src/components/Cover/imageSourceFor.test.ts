8cc21aef72feff8e220903021a3170b973978cce
Alexander Bespoyasov <forjob159@gmail.com>
2021-12-06 16:55:06 +0100

    Remove unused code

--- src/components/Cover/imageSourceFor.test.ts
+++
@@ -1,9 +0,0 @@
-import { imageSourceFor } from "./imageSourceFor";
-import { metadata } from "@shared/stubs";
-
-describe("when given an image and an extension", () => {
-  it("should compose a correct url for that image", () => {
-    const entity = { ...metadata, slug: "/test" };
-    expect(imageSourceFor(entity, "jpg")).toEqual("/img/test/cover.jpg");
-  });
-});
