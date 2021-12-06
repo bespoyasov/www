b13790154a0b3463ef3b00affb6d58624e05443e
Alexander Bespoyasov <forjob159@gmail.com>
2021-12-06 14:58:04 +0100

    Change URL hash value to be equal to the current `Switch` element's `id`
    Makes it possible to systematize the positioning polciy after a click on a control.
    
    It's difficult to calculate the precise target position after the code blocks has changes on the page.
    
    Instead, we always scroll to the current code block's begining.
    
    Works as if we used links with anchors.

--- src/components/Switch/Control.tsx
+++ src/components/Switch/Control.tsx
@@ -1,24 +1,30 @@
 import type { CodeSamplesLanguage } from "@domain/preferences";
 import { nameOf } from "@domain/preferences";
+
+import { useHashUpdate } from "@effects/useHashUpdate";
 import { usePreferencesContext } from "@global/context";
+
+import type { InstanceId } from "./types";
 import styles from "./Control.module.css";
 
 type ControlDisabled = boolean;
 type ControlProps = {
   language: CodeSamplesLanguage;
   disabled: ControlDisabled;
+  scope: InstanceId;
 };
 
-export const Control = ({ language, disabled }: ControlProps) => {
+export const Control = ({ language, disabled, scope }: ControlProps) => {
   const { updateLanguage } = usePreferencesContext();
+  const updateHash = useHashUpdate();
+
+  function applyChanges() {
+    updateLanguage(language);
+    updateHash(scope);
+  }
 
   return (
-    <button
-      type="button"
-      disabled={disabled}
-      className={styles.control}
-      onClick={() => updateLanguage(language)}
-    >
+    <button type="button" disabled={disabled} className={styles.control} onClick={applyChanges}>
       {nameOf(language)}
     </button>
   );
