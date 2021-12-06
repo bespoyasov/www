084b03192cd4f6b0f9c0579031038482b4ce501d
Alexander Bespoyasov <forjob159@gmail.com>
2021-12-06 16:47:21 +0100

    Excapsulate cover styles in the `Cover` component

--- src/components/Cover/Cover.tsx
+++ src/components/Cover/Cover.tsx
@@ -2,6 +2,7 @@ import { ImageExtension } from "@domain/image";
 import { Metadata } from "@domain/metadata";
 import { Picture } from "@components/Picture";
 import { imageSourceFor } from "./imageSourceFor";
+import styles from "./Cover.module.css";
 
 type CoverProps = {
   for: Metadata;
@@ -11,5 +12,10 @@ type CoverProps = {
 export const Cover = ({ for: entity, withExtension = "png" }: CoverProps) => {
   const { title } = entity;
   const source = imageSourceFor(entity, withExtension);
-  return <Picture src={source} alt={title} />;
+
+  return (
+    <div className={styles.cover}>
+      <Picture src={source} alt={title} />
+    </div>
+  );
 };
