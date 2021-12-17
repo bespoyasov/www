003029f2ae9e02627b6fff57456ff13704453c54
Alexander Bespoyasov <forjob159@gmail.com>
2021-12-17 14:08:04 +0100

    Remove emojis from the social card title
    The same emojis are displayed on the default social card preview image.

--- src/components/SummaryCard/const.ts
+++ src/components/SummaryCard/const.ts
@@ -1,6 +1,6 @@
 import type { SummaryCard } from "./types";
 
-const title = "Саша Беспоясов · ✍️ 💻 👋";
+const title = "Саша Беспоясов";
 const description = "Блог о разработке и около того.";
 const cover = "/img/common/social-media-preview.png";
 
