f391ae9f29a499701a4ffa689739af9b2a26ba53
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-06 16:01:21 +0200

    Extract `imageExtensions` into “value constants”

---
+++ src/core/image/const.ts
@@ -0,0 +1,3 @@
+import type { ImageExtension } from "./types";
+
+export const imageExtensions: List<ImageExtension> = ["png", "jpg", "webp", "avif"] as const;
--- src/core/image/types.ts
+++ src/core/image/types.ts
@@ -1,5 +1,3 @@
 export type ImageDescription = string;
 export type ImageFileSource = RelativePath;
 export type ImageExtension = "png" | "jpg" | "webp" | "avif";
-
-export const imageExtensions = ["png", "jpg", "webp", "avif"] as const;
