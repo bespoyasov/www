bed83822d831dde3b80161178877eff4c3e59bb8
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-06 16:21:28 +0200

    Introduce a `site` module in `core`

---
+++ src/core/site/types.ts
@@ -0,0 +1,11 @@
+export type BaseUrl = AbsoluteUrl;
+export type PageUrl = AbsoluteUrl;
+
+export type DomainZone = "ru";
+
+export type FoundationYear = FullYear;
+
+export type Site = {
+  baseUrl: BaseUrl;
+  createdAt: FoundationYear;
+};
