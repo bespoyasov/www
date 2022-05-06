9b13bd5b35329ec289d79aa2f1507e05d8716b5c
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-06 15:47:20 +0200

    Extract collection types into `collections.d.ts`

--- src/shared/types/structures.d.ts
+++ src/shared/types/structures.d.ts
@@ -1,10 +1,2 @@
-type List<TEntity> = TEntity[];
-type Dict<TValue> = Record<string, TValue>;
-type Collection<TValue> = List<TValue> | Dict<TValue>;
-
 type AnyComparable = string | number;
 type Comparable<TEntity extends AnyComparable> = TEntity;
-
-type AnyObject = Dict<unknown>;
-type AnyList = List<unknown>;
-type AnyCollection = Collection<unknown>;
