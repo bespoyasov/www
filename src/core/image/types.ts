2080e6bf1958b3af86832bd9e299e71fa147627d
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-06 16:00:32 +0200

    Specify the `ImageExtension` type as a union

--- src/core/image/types.ts
+++ src/core/image/types.ts
@@ -1,5 +1,5 @@
 export type ImageDescription = string;
 export type ImageFileSource = RelativePath;
-export type ImageExtension = typeof imageExtensions[number];
+export type ImageExtension = "png" | "jpg" | "webp" | "avif";
 
 export const imageExtensions = ["png", "jpg", "webp", "avif"] as const;
