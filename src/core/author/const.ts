ff28be6ac3bdc350ecf539bd0303fba9cfeaedf3
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-06 16:29:40 +0200

    Create an `author` constant [skip ci]

---
+++ src/core/author/const.ts
@@ -0,0 +1,17 @@
+import type { Author } from "./types";
+import { site } from "@core/site";
+
+export const author: Author = {
+  name: "Саша",
+  title: "Беспоясов ",
+  site: site.baseUrl,
+  email: "bespoyasov@me.com",
+
+  employer: "https://0x.se/team/alexander-bespoyasov.html",
+  telegram: `https://telegram.me/bespoyasov`,
+  twitter: `https://twitter.com/bespoyasov`,
+  github: `https://github.com/bespoyasov`,
+  linked: `https://www.linkedin.com/in/bespoyasov/`,
+
+  donate: `https://buymeacoffee.com/bespoyasov`,
+};
