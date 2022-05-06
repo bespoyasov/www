0f7ce382ba85e1066b26f081d2de791aea0909d3
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-06 16:06:23 +0200

    Introduce language names and language collection

--- src/core/code/types.ts
+++ src/core/code/types.ts
@@ -1,5 +1,9 @@
 type ProgrammingLanguageExtension = "js" | "ts" | "py" | "fs";
+type ProgrammingLanguageName = "JavaScript" | "TypeScript" | "Python" | "F#";
 
 export type LanguageKind = ProgrammingLanguageExtension;
+export type LanguageName = ProgrammingLanguageName;
+export type LanguageCollection = Dict<LanguageKind, LanguageName>;
 
 export type CodeSampleLanguage = LanguageKind;
+export type CodeSampleHeadline = LanguageName;
