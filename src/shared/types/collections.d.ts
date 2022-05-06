82d8e807d277ae362b041c729cba93f57754618a
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-06 15:50:16 +0200

    Replace `Any` prefix with `Some`
    The type consumers will be updated later.

--- src/shared/types/collections.d.ts
+++ src/shared/types/collections.d.ts
@@ -2,6 +2,6 @@ type List<TValue> = Readonly<TValue[]>;
 type Dict<TKey, TValue> = Readonly<Record<TKey, TValue>>;
 type Collection<TValue> = List<TValue> | Dict<string, TValue>;
 
-type AnyList = List<unknown>;
-type AnyObject = Dict<string, unknown>;
-type AnyCollection = Collection<unknown>;
+type SomeList = List<unknown>;
+type SomeDict = Dict<string, unknown>;
+type SomeCollection = Collection<unknown>;
