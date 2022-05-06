f1f535ab7dab8d257019220e926b86a927b13747
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-06 16:07:46 +0200

    Create a programming language collection
    Fill it up with languages used of somehow referred to in the blog.

---
+++ src/core/code/const.ts
@@ -0,0 +1,8 @@
+import type { LanguageCollection } from "./types";
+
+export const languages: LanguageCollection = {
+  js: "JavaScript",
+  ts: "TypeScript",
+  py: "Python",
+  fs: "F#",
+} as const;
