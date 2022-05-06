7773e8b99f3cd7bf2a6b41cc2d4d65b4206d06f8
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-06 16:11:31 +0200

    Mark the `PostContents` as a localized string

--- src/core/post/types.ts
+++ src/core/post/types.ts
@@ -1,5 +1,5 @@
 type SourceFileName = string;
 
 export type PostId = SourceFileName;
-export type PostContents = string;
+export type PostContents = LocalizedString;
 export type PostKind = "projects" | "blog";
