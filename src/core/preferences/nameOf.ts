cabfb5428c3c0dc5f4b614f65b70a5d98cd5da9e
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-06 16:09:07 +0200

    Move `nameOf` from `preferences` to `code`
    Consumers will be updated later.

--- src/core/preferences/nameOf.ts
+++
@@ -1,6 +0,0 @@
-import type { CodeSamplesLanguage } from "./types";
-import { ProgrammingLanguageName } from "./const";
-
-export function nameOf(key: CodeSamplesLanguage): ProgrammingLanguageName {
-  return ProgrammingLanguageName[key];
-}
