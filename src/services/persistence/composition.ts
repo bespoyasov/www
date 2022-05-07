832e3a773d65c297a03de45b46b73feabb728c33
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 14:31:43 +0200

    Introduce a dependency on the locale

--- src/services/persistence/composition.ts
+++ src/services/persistence/composition.ts
@@ -1,9 +1,12 @@
 import system from "fs";
+import { currentLocale as locale } from "@env/locale";
 
 export type Dependencies = {
   system?: typeof system;
+  locale?: Locale;
 };
 
 export const dependencies: Dependencies = {
   system,
+  locale,
 };
