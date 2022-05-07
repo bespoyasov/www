91cc712baa3ab82e2c82a2ca32467e70479a3f8b
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 17:46:06 +0200

    Extract the projects page static view into `@views`

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,12 +1,9 @@
-import Head from "next/head";
 import { GetStaticProps } from "next";
-
-import { SummaryCard } from "@components/SummaryCard";
-import { Description } from "@components/Description";
-import { Showcase } from "@components/Showcase";
 
 import { Metadata } from "@domain/metadata";
 import { projectsMetadata } from "@network/fetch";
+
+import { Projects as ProjectsPage } from "@views/Projects";
 
 type ProjectsProps = {
   projects: Metadata[];
@@ -20,19 +17,4 @@ export const getStaticProps: GetStaticProps<ProjectsProps> = () => {
   };
 };
 
-const Projects = ({ projects }: ProjectsProps) => {
-  return (
-    <>
-      <Head>
-        <title>Сделал</title>
-        <Description>
-          Последние сайты, приложения, книги и образовательные проекты, над которыми я работал.
-        </Description>
-        <SummaryCard />
-      </Head>
-      <Showcase projects={projects} />
-    </>
-  );
-};
-
-export default Projects;
+export default ProjectsPage;
---
+++ src/ui/views/Projects/types.ts
@@ -0,0 +1,5 @@
+import type { Metadata } from "@core/metadata";
+
+export type ProjectsProps = {
+  projects: List<Metadata>;
+};
