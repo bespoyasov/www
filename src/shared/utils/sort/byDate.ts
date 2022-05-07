5593b361c6d70933169c7c93473d281857632184
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 11:43:32 +0200

    Use updated sorter factory, import `types` when neccessary

--- src/shared/utils/sort/byDate.ts
+++ src/shared/utils/sort/byDate.ts
@@ -1,11 +1,13 @@
-import { Metadata } from "@domain/metadata";
-import { SortDirection } from "./types";
-import { sortWith } from "./sortWith";
+import type { Metadata } from "@core/metadata";
+import type { Comparable } from "./types";
 
-function extractComparableDate({ datetime }: Metadata): Comparable<UnixTimeStamp> {
+import { SortOrder } from "./const";
+import { sorterFor } from "./factory";
+
+function comparableDateTime({ datetime }: Metadata): Comparable<UnixTimeStamp> {
   if (!datetime) throw new Error("Failed to sort by missing datetime field.");
   return Date.parse(datetime);
 }
 
-export const byDate = sortWith(extractComparableDate);
-export const byDateDescending = sortWith(extractComparableDate, SortDirection.Descending);
+export const byDate = sorterFor(comparableDateTime);
+export const byDateDescending = sorterFor(comparableDateTime, SortOrder.Descending);
