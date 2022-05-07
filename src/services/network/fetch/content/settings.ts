6a060ee5f62b3f157084687c4b885554311eb878
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 19:32:33 +0200

    Remove unused composition and settings in `network` [skip ci]

--- src/services/network/fetch/content/settings.ts
+++
@@ -1,7 +0,0 @@
-import { SerializeOptions } from "next-mdx-remote/dist/types";
-import { serializationConfig } from "@configs/serialization";
-import { assureType } from "@utils/assureType";
-
-export const settings: SerializeOptions = {
-  mdxOptions: assureType(serializationConfig),
-};
