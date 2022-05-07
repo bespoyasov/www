1fc6e55096219a4cdbd0f3f2d2c7ece8f66f9d1a
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 15:38:27 +0200

    Update social media preview image path

--- src/ui/components/SummaryCard/const.ts
+++ src/ui/components/SummaryCard/const.ts
@@ -2,6 +2,7 @@ import type { SummaryCard } from "./types";
 import { translated } from "@translation";
 
 const { title, description } = translated.summaryCard;
+const cover = "/images/static/social-media-preview.png";
 
 export const DEFAULT_CARD: SummaryCard = {
   title,
