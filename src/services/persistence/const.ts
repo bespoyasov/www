7d4f5c93e99087ca624f44f53bed0618f6fba33b
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 14:31:21 +0200

    Introduce `BASE_DIRECTORY`, remove static blog and projects directiries since they are going to depend on the locale

--- src/services/persistence/const.ts
+++ src/services/persistence/const.ts
@@ -1,5 +1,4 @@
 import path from "path";
 
 export const ROOT_DIRECTORY = process.cwd();
-export const BLOG_DIRECTORY = path.join(ROOT_DIRECTORY, "data/blog");
-export const PROJECTS_DIRECTORY = path.join(ROOT_DIRECTORY, "data/projects");
+export const BASE_DIRECTORY = path.join(ROOT_DIRECTORY, "storage");
