e96d8dc003af4d1f21d44687d23a25feb3a481c8
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 17:09:01 +0200

    Localize the `NotFound` view content

--- src/ui/views/NotFound/NotFound.tsx
+++ src/ui/views/NotFound/NotFound.tsx
@@ -1,11 +1,12 @@
+import { translated } from "@translation";
 import styles from "./NotFound.module.css";
 
 export const NotFound = () => {
   return (
     <main className={styles.container}>
       <article>
-        <h1>404</h1>
-        <p>Упс! Такой страницы нет :–(</p>
+        <h1>{translated.notFound.title}</h1>
+        <p>{translated.notFound.description}</p>
       </article>
     </main>
   );
