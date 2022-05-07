32374fa25b08274d138e0b436818b86a504bf96e
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 17:29:54 +0200

    Localize the blog page static content

--- src/ui/views/Blog/Blog.tsx
+++ src/ui/views/Blog/Blog.tsx
@@ -6,29 +6,28 @@ import { SummaryCard } from "@components/SummaryCard";
 import { Section } from "@components/Section";
 import { Anthology, Digest, Subscribe } from "@components/Notes";
 
+import { translated } from "@translation";
 import type { BlogProps } from "./types";
 
 export const Blog = ({ posts }: BlogProps) => {
   return (
     <main>
       <Head>
-        <title>Написал</title>
-        <Description>
-          Свежие статьи из блога, подборки заметок по тегам и архив всех статей с 2012 года.
-        </Description>
+        <title>{translated.blogPage.title}</title>
+        <Description>{translated.blogPage.description}</Description>
         <SummaryCard />
       </Head>
 
-      <VisuallyHidden as="h1">Все заметки</VisuallyHidden>
+      <VisuallyHidden as="h1">{translated.blogPage.heading}</VisuallyHidden>
 
-      <Section title="Свежее">
-        <p>Последние статьи из блога и подборки по тегам.</p>
+      <Section title={translated.blogPage.recentTitle}>
+        <p>{translated.blogPage.recentDescription}</p>
         <Digest notes={posts} />
         <Subscribe />
       </Section>
 
-      <Section title="Всё подряд">
-        <p>Все опубликованные статьи от новых к старым.</p>
+      <Section title={translated.blogPage.anthologyTitle}>
+        <p>{translated.blogPage.anthologyDescription}</p>
         <Anthology notes={posts} />
       </Section>
     </main>
