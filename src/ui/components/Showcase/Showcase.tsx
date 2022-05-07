6f0873e7a39130115a29c2b00ef578b6fa2a8f36
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 17:48:31 +0200

    Localize the `Showcase` component contents

--- src/ui/components/Showcase/Showcase.tsx
+++ src/ui/components/Showcase/Showcase.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "@core/metadata";
 
 import { ProjectsList } from "@components/ProjectsList";
+import { translated } from "@translation";
 import styles from "./Showcase.module.css";
 
 type ShowcaseProps = {
@@ -10,8 +11,8 @@ type ShowcaseProps = {
 export const Showcase = ({ projects }: ShowcaseProps) => {
   return (
     <main className={styles.showcase}>
-      <h1>Проекты</h1>
-      <p>Последние сайты, приложения, книги и образовательные проекты, над которыми я работал.</p>
+      <h1>{translated.showcase.heading}</h1>
+      <p>{translated.showcase.description}</p>
       <ProjectsList projects={projects} />
     </main>
   );
