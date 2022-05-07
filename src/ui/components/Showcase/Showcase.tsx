cd34bb4bd1065287cf351ecc604be11ff1855cd9
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 17:48:02 +0200

    Use type imports in `Showcase`

--- src/ui/components/Showcase/Showcase.tsx
+++ src/ui/components/Showcase/Showcase.tsx
@@ -1,4 +1,5 @@
-import { Metadata } from "@domain/metadata";
+import type { Metadata } from "@core/metadata";
+
 import { ProjectsList } from "@components/ProjectsList";
 import styles from "./Showcase.module.css";
 
