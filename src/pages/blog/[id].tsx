0175eaa860162205753f4e048d17368e43de06cb
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 18:27:43 +0200

    Update imports, use extracted view for rendering the post

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,62 +1,30 @@
-import Head from "next/head";
-import { GetStaticProps, GetStaticPaths } from "next";
+import type { GetStaticProps, GetStaticPaths } from "next";
+import type { PostProps } from "@views/Post";
 
-import { PostContents } from "@domain/post";
-import { Metadata as MetadataType } from "@domain/metadata";
-import { blogPostsMetadata, blogPostsNames, fetchBlogPost } from "@network/fetch";
+import { notesMetadata, noteNames, fetchNote } from "@network/fetch";
+import { PostView as BlogPost } from "@views/Post";
 
-import { Post } from "@components/Post";
-import { Adjacent } from "@components/Adjacent";
-import { Metadata } from "@components/Metadata";
-import { Feedback } from "@components/Feedback";
-import { Description } from "@components/Description";
-import { SummaryCard } from "@components/SummaryCard";
-
-type BlogPostProps = {
-  contents: PostContents;
-  metadata: MetadataType;
-  prevPost: Nullable<MetadataType>;
-  nextPost: Nullable<MetadataType>;
-};
-
-export const getStaticProps: GetStaticProps<BlogPostProps> = async (context) => {
+export const getStaticProps: GetStaticProps<PostProps> = async (context) => {
   const postId = String(context.params.id);
 
-  const content = await fetchBlogPost(postId);
-  const blogPosts = blogPostsMetadata();
-  const index = blogPosts.findIndex((post) => post.slug.endsWith(postId));
+  const content = await fetchNote(postId);
+  const posts = notesMetadata();
+  const index = posts.findIndex((post) => post.slug.endsWith(postId));
 
   return {
     props: {
-      metadata: blogPosts[index],
-      prevPost: blogPosts[index + 1] ?? null,
-      nextPost: blogPosts[index - 1] ?? null,
+      metadata: posts[index],
+      prevPost: posts[index + 1] ?? null,
+      nextPost: posts[index - 1] ?? null,
       contents: content,
     },
   };
 };
 
 export const getStaticPaths: GetStaticPaths = () => {
-  const blogPosts = blogPostsNames();
-  const paths = blogPosts.map((id) => ({ params: { id } }));
+  const posts = noteNames();
+  const paths = posts.map((id) => ({ params: { id } }));
   return { paths, fallback: false };
-};
-
-const BlogPost = ({ metadata, prevPost, nextPost, contents }: BlogPostProps) => {
-  return (
-    <>
-      <Head>
-        <title>{metadata.title}</title>
-        <Description>{metadata.description}</Description>
-        <SummaryCard metadata={metadata} />
-      </Head>
-
-      <Post content={contents} />
-      <Feedback metadata={metadata} />
-      <Metadata metadata={metadata} />
-      <Adjacent prev={prevPost} next={nextPost} />
-    </>
-  );
 };
 
 export default BlogPost;
