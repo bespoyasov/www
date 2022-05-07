306a28108395ff7103b10d863b656e0480486fd2
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 20:34:41 +0200

    Select the language when creating a new post

--- ops/_templates/post/new/prompt.js
+++ ops/_templates/post/new/prompt.js
@@ -1,5 +1,11 @@
 module.exports = [
   {
+    type: "select",
+    name: "locale",
+    message: "Select the post language:",
+    choices: ["en", "ru"],
+  },
+  {
     type: "input",
     name: "title",
     message: "Enter the post title:",
