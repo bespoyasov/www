e4f5ed7bd37e48028e31aa4877bc8249cc213d5d
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 14:28:08 +0200

    Replace `"blog"` with `"notes"` in possible query kind

--- src/services/persistence/types.ts
+++ src/services/persistence/types.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 
 export type SystemType = typeof fs;
 
-export type QueryKind = "blog" | "projects";
+export type QueryKind = "notes" | "projects";
 export type FileExtension = "mdx" | "tsx";
 export type FileName = AbsolutePath;
 export type FileContent = LocalizedString;
