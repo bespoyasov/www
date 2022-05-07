15dd707360dde5a6235dd2536f17e7b0ae7426c3
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 11:31:52 +0200

    Use `it.each` for better console test result output

--- src/shared/utils/exists/index.test.ts
+++ src/shared/utils/exists/index.test.ts
@@ -1,15 +1,17 @@
 import { exists } from ".";
 
-describe("for a truthy value", () => {
-  it("should return `true`", () => {
-    const truthyValues = [42, -1, "test", "0", true, [], [42], {}, { a: 42 }, Infinity, -Infinity];
-    truthyValues.forEach((value) => expect(exists(value)).toEqual(true));
+describe("when given a truthy value", () => {
+  const truthyValues = [42, -1, "test", "0", true, [], [42], {}, { a: 42 }, Infinity, -Infinity];
+
+  it.each(truthyValues)("should return `true` for %p", (value) => {
+    expect(exists(value)).toEqual(true);
   });
 });
 
-describe("for a falsy value", () => {
-  it("should return `false`", () => {
-    const falsyValues = [0, "", false, null, undefined, NaN];
-    falsyValues.forEach((value) => expect(exists(value)).toEqual(false));
+describe("when given a falsy value", () => {
+  const falsyValues = [0, "", false, null, undefined, NaN];
+
+  it.each(falsyValues)("should return `false` for %p", (value) => {
+    expect(exists(value)).toEqual(false);
   });
 });
