4f7d35cf55b633be279f4b9f230713c3b30b17a8
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 14:29:45 +0200

    Remove `SystemType` as it's not used anymore

--- src/services/persistence/types.ts
+++ src/services/persistence/types.ts
@@ -1,7 +1,3 @@
-import fs from "fs";
-
-export type SystemType = typeof fs;
-
 export type QueryKind = "notes" | "projects";
 export type FileExtension = "mdx" | "tsx";
 export type FileName = AbsolutePath;
