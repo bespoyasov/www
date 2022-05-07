03320fdd00c2483c0f3eadf29c5e3533ec26e031
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 19:09:15 +0200

    Update imports for the travel page

--- src/pages/tag/travel.tsx
+++ src/pages/tag/travel.tsx
@@ -1,12 +1,12 @@
-import { GetStaticProps } from "next";
-import { withTravelTag } from "@utils/filter";
-import { blogPostsMetadata } from "@network/fetch";
-
+import type { GetStaticProps } from "next";
 import type { TravelProps } from "@views/Travel";
+
+import { withTravelTag } from "@utils/filter";
+import { notesMetadata } from "@network/fetch";
 import { Travel as TravelPage } from "@views/Travel";
 
 export const getStaticProps: GetStaticProps<TravelProps> = () => {
-  const allPosts = blogPostsMetadata();
+  const allPosts = notesMetadata();
   const travelPosts = allPosts.filter(withTravelTag);
 
   return {
