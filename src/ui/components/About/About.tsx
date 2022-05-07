256700f7b33049824e837e31e2b7f154b95946f2
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 16:11:31 +0200

    Localize the `About` component

--- src/ui/components/About/About.tsx
+++ src/ui/components/About/About.tsx
@@ -1,28 +1,17 @@
 import { VisuallyHidden } from "@components/VisuallyHidden";
+import { translated } from "@translation";
+
 import styles from "./About.module.css";
 
 export const About = () => {
   return (
     <main className={styles.about}>
-      <VisuallyHidden as="h2">Обо мне</VisuallyHidden>
-      <div>
-        <p>Разработчик из Стокгольма.</p>
-        <p>
-          Превращаю идеи и макеты в сайты и приложения. Помогаю доводить проекты до конца
-          и запускать их в срок.
-        </p>
-        <p>Поддерживаю и развиваю продукты компаний и собственные проекты.</p>
-      </div>
-      <div>
-        <p>
-          Рассказываю в блоге о процессе и результатах работы. Пишу о дизайне, разработке и книгах.
-        </p>
-        <p>Менторю и помогаю начинающим технологам освоить разработку.</p>
-        <p>
-          Иногда пишу в <a href="https://twitter.com/bespoyasov">Твиттер</a> и коммичу на 
-          <a href="https://github.com/bespoyasov">Гитхаб</a>.
-        </p>
-      </div>
+      <VisuallyHidden as="h2">{translated.about.heading}</VisuallyHidden>
+
+      <p>{translated.about.introduction}</p>
+      <p>{translated.about.projects}</p>
+      <p>{translated.about.blogging}</p>
+      <p>{translated.about.teaching}</p>
     </main>
   );
 };
