dbbaad38be0ae469cd533b353e118390a32958bc
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 14:54:25 +0200

    Register dependencies and settings for the module

--- src/services/network/composition.ts
+++ src/services/network/composition.ts
@@ -1,16 +1,40 @@
 import type { GrayMatterFile } from "gray-matter";
 import type { MDXRemoteSerializeResult } from "next-mdx-remote";
-import type { SerializeOptions } from "next-mdx-remote/dist/types";
+import type { SerializeOptions as SerializeSettings } from "next-mdx-remote/dist/types";
+
+import parse from "gray-matter";
+import { serialize } from "next-mdx-remote/serialize";
+
+import { serializationConfig } from "@configs/serialization";
+import { assureType } from "@utils/assureType";
 
 type SerializeSource = string;
 type SerializeResult = MDXRemoteSerializeResult;
-
-export type Serializer = (
+type Serializer = (
   source: SerializeSource,
-  options?: SerializeOptions,
+  options?: SerializeSettings,
 ) => Promise<SerializeResult>;
 
 type ParseSource = string;
 type ParseResult = GrayMatterFile<string>;
+type Parser = (source: ParseSource) => ParseResult;
 
-export type Parser = (source: ParseSource) => ParseResult;
+export type Dependencies = {
+  serialize?: Serializer;
+  parse?: Parser;
+};
+
+export const dependencies: Dependencies = {
+  serialize,
+  parse,
+};
+
+export type Settings = {
+  serializeSettings: SerializeSettings;
+};
+
+export const settings: Settings = {
+  serializeSettings: {
+    mdxOptions: assureType(serializationConfig),
+  },
+};
