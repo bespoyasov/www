c02d025fe9d1e003dedf17f1d76fdcb25146c6ff
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 18:57:50 +0200

    Update imports in `TagCatalogue`

--- src/ui/components/TagCatalogue/TagCatalogue.tsx
+++ src/ui/components/TagCatalogue/TagCatalogue.tsx
@@ -1,10 +1,13 @@
-import { Tag, TagKind } from "@domain/tags";
+import type { TagKind } from "@core/tags";
+import { tags } from "@core/tags";
+
 import { byTagValue } from "@utils/sort";
 import { splitInto } from "@utils/splitInto";
 import { TagLink } from "@components/TagLink";
-import styles from "./AllTags.module.css";
+import styles from "./TagCatalogue.module.css";
 
 export const TagCatalogue = () => {
+  const allTags = Object.keys(tags).sort(byTagValue);
 
   const columnsCount = 2;
   const columnsContent = splitInto(allTags, columnsCount);
