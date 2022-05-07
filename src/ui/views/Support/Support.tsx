a3a580a4422e5c9ca5dc13a11365c8ddef2c97ca
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 17:21:38 +0200

    Localize the support page content

--- src/ui/views/Support/Support.tsx
+++ src/ui/views/Support/Support.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { SupportOptions } from "@components/SupportOptions";
 import { Description } from "@components/Description";
 import { SummaryCard } from "@components/SummaryCard";
+import { translated } from "@translation";
 
 import styles from "./Support.module.css";
 
@@ -10,26 +11,16 @@ export const SupportMe = () => {
   return (
     <main className={styles.support}>
       <Head>
-        <title>Сказать спасибо ❤️</title>
-        <Description>Как поддержать меня и мой блог.</Description>
+        <title>{translated.supportPage.title}</title>
+        <Description>{translated.supportPage.description}</Description>
         <SummaryCard />
       </Head>
 
-      <h1>Сказать спасибо</h1>
-      <p>Блог — это моё хобби.</p>
-      <p>
-        Я нахожу в ежедневной работе то, что может пригодиться другим разработчикам. Опыт, грабли
-        и шишки я превращаю в статьи и иногда в онлайн-книги.
-      </p>
-      <p>
-        Мне нравится думать, что мой труд полезен читателям. Это мотивирует искать интересное
-        в рабочей рутине, находить и конспектировать хорошие книги о разработке, рассказывать
-        о неудачах и как их можно избегать.
-      </p>
-      <p>
-        Если вам нравится мой блог, и вы хотите поддержать выход новых статей и проектов, дайте
-        знать — я буду благодарен.
-      </p>
+      <h1>{translated.supportPage.title}</h1>
+      <p>{translated.supportPage.introduction}</p>
+      <p>{translated.supportPage.experience}</p>
+      <p>{translated.supportPage.motivation}</p>
+      <p>{translated.supportPage.feedback}</p>
 
       <SupportOptions />
     </main>
