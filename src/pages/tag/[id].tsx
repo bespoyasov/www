0ca276dd2fdf45a59c1f6f47a0641055fd0c9c52
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 19:02:08 +0200

    Update data fetcher functions, update imports in on the tag page

--- src/pages/tag/[id].tsx
+++ src/pages/tag/[id].tsx
@@ -1,8 +1,10 @@
-import { GetStaticProps, GetStaticPaths } from "next";
+import type { GetStaticProps, GetStaticPaths } from "next";
+import type { TagKind } from "@core/tags";
+import { tags } from "@core/tags";
+
+import { projectsMetadata, notesMetadata } from "@network/fetch";
 import { assureType } from "@utils/assureType";
 import { withTag } from "@utils/filter";
-import { Tag as TagEnum, TagKind } from "@domain/tags";
-import { projectsMetadata, blogPostsMetadata } from "@network/fetch";
 
 import type { TagProps } from "@views/Tag";
 import { Tag as TagPage } from "@views/Tag";
@@ -20,7 +22,7 @@ export const getStaticProps: GetStaticProps<TagProps> = ({ params }) => {
   const byTag = withTag(desired);
 
   const projects = projectsMetadata().filter(byTag);
-  const notes = blogPostsMetadata().filter(byTag);
+  const notes = notesMetadata().filter(byTag);
 
   return {
     props: {
@@ -31,7 +33,7 @@ export const getStaticProps: GetStaticProps<TagProps> = ({ params }) => {
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = Object.keys(TagEnum).filter(excludeTravel).map(pathFromTag);
+  const paths = Object.keys(tags).filter(excludeTravel).map(pathFromTag);
   return { paths, fallback: false };
 };
 
