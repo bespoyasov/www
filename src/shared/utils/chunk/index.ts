c6e6380ff57f41609b1bf3c9af56a138a2b062df
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 11:31:00 +0200

    Use `readonly` to make the resulting lists compatible with the signature

--- src/shared/utils/chunk/index.ts
+++ src/shared/utils/chunk/index.ts
@@ -1,11 +1,12 @@
 import { isEmpty } from "@utils/isEmpty";
+import { readonly } from "@utils/readonly";
 
 type ChunkSize = number;
 
 export function chunk<TList extends SomeList>(array: TList, size: ChunkSize): List<TList> {
   if (isEmpty(array)) return [];
 
-  const head = array.slice(0, size) as TList;
-  const tail = array.slice(size) as TList;
+  const head = readonly(array.slice(0, size)) as TList;
+  const tail = readonly(array.slice(size)) as TList;
   return [head, ...chunk(tail, size)];
 }
