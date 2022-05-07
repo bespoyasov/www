4788cffaee231ecf738aa624701d395e01949879
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 15:44:56 +0200

    Remove unused components after `Footer` update

--- src/ui/components/Contacts/Contacts.tsx
+++
@@ -1,7 +0,0 @@
-export const Contacts = () => {
-  return (
-    <a href="mailto:bespoyasov@me.com" className="text-color">
-      bespoyasov@me.com
-    </a>
-  );
-};
--- src/ui/components/Since/Since.tsx
+++
@@ -1,3 +0,0 @@
-export const Since = () => {
-  return <div>2010+ 🇸🇪</div>;
-};
