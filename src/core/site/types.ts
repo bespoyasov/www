2a6beec31f7bb6bb251c2b93534b5d5f575d1bc1
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 11:53:09 +0200

    Extend `DomainZone` with `.me` value

--- src/core/site/types.ts
+++ src/core/site/types.ts
@@ -1,7 +1,7 @@
 export type BaseUrl = AbsoluteUrl;
 export type PageUrl = AbsoluteUrl;
 
-export type DomainZone = "ru";
+export type DomainZone = "me" | "ru";
 
 export type FoundationYear = FullYear;
 
