1b3a874ff321d6c030234cbc55c5086eb415f60d
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 11:48:21 +0200

    Localize the author info

--- src/core/author/const.ts
+++ src/core/author/const.ts
@@ -1,15 +1,16 @@
 import type { Author } from "./types";
 import { site } from "@core/site";
+import { translated } from "@translation";
 
 export const author: Author = {
-  name: "Саша",
-  title: "Беспоясов ",
+  name: translated.author.name,
+  title: translated.author.title,
   site: site.baseUrl,
   email: "bespoyasov@me.com",
 
   employer: "https://0x.se/team/alexander-bespoyasov.html",
   telegram: `https://telegram.me/bespoyasov`,
-  twitter: `https://twitter.com/bespoyasov`,
+  twitter: `https://twitter.com/${translated.author.twitter}`,
   github: `https://github.com/bespoyasov`,
   linked: `https://www.linkedin.com/in/bespoyasov/`,
 
