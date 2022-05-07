5ffb2e7fc9dca20657fe7fe607b5ee42ae28fa4d
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-07 16:04:48 +0200

    Use localized link titles in `Nav`

--- src/ui/components/Nav/routes.ts
+++ src/ui/components/Nav/routes.ts
@@ -1,8 +1,21 @@
 import type { NavRoute } from "./types";
+import { translated } from "@translation";
 
 export const routes: List<NavRoute> = [
-  { url: "/projects", title: "Проекты" },
-  { url: "/blog", title: "Блог" },
-  { url: "/talks", title: "Доклады" },
-  { url: "/tag/favorite", title: "Лучшее" },
+  {
+    url: "/projects",
+    title: translated.nav.projects,
+  },
+  {
+    url: "/blog",
+    title: translated.nav.blog,
+  },
+  {
+    url: "/talks",
+    title: translated.nav.talks,
+  },
+  {
+    url: "/tag/favorite",
+    title: translated.nav.favorite,
+  },
 ];
