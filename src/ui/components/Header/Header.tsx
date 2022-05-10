359a6e8db5c1ff6d7910d4d8a30f7a9814ff16a2
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-10 10:48:48 +0200

    Render language toggle in `Header` [skip ci]

--- src/ui/components/Header/Header.tsx
+++ src/ui/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import { onMainPage } from "@utils/onMainPage";
+import { Language } from "@components/Language";
 import { Author } from "@components/Author";
 import { Link } from "@components/LinkIf";
 import { Nav } from "@components/Nav";
@@ -15,6 +16,7 @@ export const Header = () => {
       </Link>
 
       <Nav className={styles.nav} />
+      <Language className={styles.language} />
     </header>
   );
 };
