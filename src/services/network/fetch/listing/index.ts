ee84bd16b5abe5417f229196dfa0acb1e0bf16eb
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-14 14:06:52 +0200

    Extend fetch functions with talks-related ones [skip ci]

--- src/services/network/fetch/listing/index.ts
+++ src/services/network/fetch/listing/index.ts
@@ -1,7 +1,8 @@
 import type { PostId } from "@core/post";
-import { noteList, projectList } from "@persistence/source";
+import { noteList, projectList, talkList } from "@persistence/source";
 
 type FetchNames = () => List<PostId>;
 
 export const projectNames: FetchNames = projectList;
 export const noteNames: FetchNames = noteList;
+export const talkNames: FetchNames = talkList;
--- src/services/network/fetch/metadata/index.ts
+++ src/services/network/fetch/metadata/index.ts
@@ -1,5 +1,6 @@
-import { allProjects, allNotes } from "@persistence/source";
+import { allProjects, allNotes, allTalks } from "@persistence/source";
 import { metadataFor } from "./factory";
 
 export const projectsMetadata = metadataFor(allProjects);
 export const notesMetadata = metadataFor(allNotes);
+export const talksMetadata = metadataFor(allTalks);
