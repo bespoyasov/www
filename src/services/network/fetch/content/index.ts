a706ec8c888b4c7a9800fbf40faa63a1e1147926
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-14 14:06:52 +0200

    Extend fetch functions with talks-related ones [skip ci]

--- src/services/network/fetch/content/index.ts
+++ src/services/network/fetch/content/index.ts
@@ -1,5 +1,6 @@
-import { getNote, getProject } from "@persistence/source";
+import { getNote, getProject, getTalk } from "@persistence/source";
 import { contentFor } from "./factory";
 
 export const fetchProject = contentFor(getProject);
 export const fetchNote = contentFor(getNote);
+export const fetchTalk = contentFor(getTalk);
