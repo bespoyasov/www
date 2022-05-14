c59aa8bcdec893764910c89b4522b7fc3dbb7f36
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-14 15:58:56 +0200

    Introduce a “Talks” page [skip ci]

---
+++ src/pages/talks/index.tsx
@@ -0,0 +1,15 @@
+import type { GetStaticProps } from "next";
+import type { TalksProps } from "@views/Talks";
+
+import { talksMetadata } from "@network/fetch";
+import { Talks as TalksPage } from "@views/Talks";
+
+export const getStaticProps: GetStaticProps<TalksProps> = () => {
+  return {
+    props: {
+      talks: talksMetadata()
+    },
+  };
+};
+
+export default TalksPage;
