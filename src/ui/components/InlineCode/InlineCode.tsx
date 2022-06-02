cf302dc8923ba9058505767e3aade659f18341a0
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-02 23:21:52 +0200

    Prevent inline styles interference with code block styles
    The `code` inside `pre` is assigned with classes of syntax highlight. To prevent mxing those with the inline code element styles we prefer “external” class name over the component's.

--- src/ui/components/InlineCode/InlineCode.tsx
+++ src/ui/components/InlineCode/InlineCode.tsx
@@ -1,6 +1,9 @@
+import type { HTMLAttributes } from "react";
 import type { WithChildren } from "@extensions/components";
 import styles from "./InlineCode.module.css";
 
-export const InlineCode = ({ children }: WithChildren) => {
-  return <code className={styles.code}>{children}</code>;
+type InlineCodeProps = WithChildren<HTMLAttributes<HTMLElement>>;
+
+export const InlineCode = ({ children, className }: InlineCodeProps) => {
+  return <code className={className ?? styles.code}>{children}</code>;
 };
