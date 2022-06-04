0c2642a6de4d270ce5835485a2ec253b5563d30c
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-04 22:05:22 +0200

    Remove `useHashUpdate` as it is not used anymore

--- src/ui/effects/useHashUpdate/index.ts
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/router";
-import type { HashUpdater } from "./types";
-
-export function useHashUpdate(): HashUpdater {
-  const router = useRouter();
-  return (hash) => router.push(`#${hash}`);
-}
