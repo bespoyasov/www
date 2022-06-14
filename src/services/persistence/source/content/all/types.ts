da857f88aa6bf807f2bd485b0ae6a8afce9ca29b
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-14 15:29:25 +0200

    Introduce a temporary type for content queries

--- src/services/persistence/source/content/all/types.ts
+++ src/services/persistence/source/content/all/types.ts
@@ -2,3 +2,4 @@ import type { FileContent } from "@persistence/types";
 import type { Dependencies } from "@persistence/composition";
 
 export type QueryPosts = (di?: Dependencies) => List<FileContent>;
+export type QueryPosts_ = () => List<FileContent>;
