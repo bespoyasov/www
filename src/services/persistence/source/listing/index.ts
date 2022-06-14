1679c4ef4aad9c5f31d7d8a78829215320359b40
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-14 13:58:27 +0200

    Replace older implementation with the updated one

--- src/services/persistence/source/listing/index.ts
+++ src/services/persistence/source/listing/index.ts
@@ -1,18 +1,7 @@
-import type { QueryKind } from "@persistence/types";
-import type { QueryListing } from "./types";
-
-import { directoryFor, hasMdx } from "@persistence/utils";
-import { dependencies } from "@persistence/composition";
-
-function queryFor(query: QueryKind): QueryListing {
-  return function execute({ system } = dependencies) {
-    return system
-      .readdirSync(directoryFor(query))
-      .filter(hasMdx)
-      .map((fileName) => fileName.replace(".mdx", ""));
-  };
-}
+import { queryFor } from "./composition";
 
 export const projectList = queryFor("projects");
 export const noteList = queryFor("notes");
 export const talkList = queryFor("talks");
+
+export * from "./types";
