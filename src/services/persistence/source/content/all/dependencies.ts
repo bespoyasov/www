2aa82730272a0abe2bf7e806b90a611f7f95b8fd
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-14 15:39:16 +0200

    Provide `path` as a dependency as well

--- src/services/persistence/source/content/all/dependencies.ts
+++ src/services/persistence/source/content/all/dependencies.ts
@@ -1,11 +1,16 @@
 import system from "fs";
+import path from "path";
+
+import type { PlatformPath } from "path";
 import type { QueryKind } from "@persistence/types";
 
 export type Dependencies = {
   system: typeof system;
+  path: PlatformPath;
   query: QueryKind;
 };
 
 export const dependencies: Omit<Dependencies, RuntimeSpecified<"query">> = {
   system,
+  path,
 };
