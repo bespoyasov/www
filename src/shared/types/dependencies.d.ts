76291f59eb6dbf45ac1b5e787cf4c9f48e44914b
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-14 15:41:42 +0200

    Create a `Configurable` type wrapper

--- src/shared/types/dependencies.d.ts
+++ src/shared/types/dependencies.d.ts
@@ -1,2 +1,7 @@
 type Factory<TEntity, TDependencies> = (di: TDependencies) => TEntity;
+
 type RuntimeSpecified<T> = T;
+type Configurable<TDependencies, TMembers extends keyof TDependencies> = Omit<
+  TDependencies,
+  TMembers
+>;
