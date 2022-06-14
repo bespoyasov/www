26af81ed7450ebcb6fae08d6e46ad2df8eb7940a
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-14 11:02:17 +0200

    Use the `Factory` type as the type signature

--- src/services/persistence/utils/directory/factory.ts
+++ src/services/persistence/utils/directory/factory.ts
@@ -2,7 +2,7 @@ import { BASE_DIRECTORY } from "@persistence/const";
 import type { Dependencies } from "./dependencies";
 import type { QueryDirectory } from "./types";
 
-export const createQueryDirectory =
-  ({ locale, path }: Dependencies): QueryDirectory =>
+export const createQueryDirectory: Factory<QueryDirectory, Dependencies> =
+  ({ locale, path }) =>
   (query) =>
     path.join(BASE_DIRECTORY, locale, query);
