7eeada0922e902a862b46485bf045721478da748
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-14 11:34:46 +0200

    Re-export updated `withMdx` util

--- src/services/persistence/utils/extension/with/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { withMdx } from ".";
-
-describe("when given a filename", () => {
-  it("should concatenate it with the `.mdx` extension", () => {
-    expect(withMdx("filename")).toEqual("filename.mdx");
-  });
-});
