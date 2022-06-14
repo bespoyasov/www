3141d28c04c11b8b769a2a93395b0ae86ea38ec3
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-14 15:43:04 +0200

    Remove obsolete types

--- src/shared/types/dependencies.d.ts
+++ src/shared/types/dependencies.d.ts
@@ -1,6 +1,5 @@
 type Factory<TEntity, TDependencies> = (di: TDependencies) => TEntity;
 
-type RuntimeSpecified<T> = T;
 type Configurable<TDependencies, TMembers extends keyof TDependencies> = Omit<
   TDependencies,
   TMembers
