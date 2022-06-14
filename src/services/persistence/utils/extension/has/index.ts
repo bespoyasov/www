c1e5a08ab03ae0ce68a7beb83ce37665a56c4752
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-14 11:17:32 +0200

    Replace older implementation with the updated one

--- src/services/persistence/utils/extension/has/index.ts
+++ src/services/persistence/utils/extension/has/index.ts
@@ -1,9 +1,2 @@
-import { FileExtension, FileName } from "@persistence/types";
-
-function is(extension: FileExtension) {
-  return function hasExtension(fileName: FileName): boolean {
-    return fileName.endsWith(`.${extension}`);
-  };
-}
-
-export const hasMdx = is("mdx");
+export * from "./types";
+export * from "./composition";
