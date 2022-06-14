496b46fbc974a1889d2be0813f430f8647c3d98d
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-14 15:32:26 +0200

    Specify `query` as a runtime dependency

--- src/services/persistence/source/content/all/dependencies.ts
+++ src/services/persistence/source/content/all/dependencies.ts
@@ -1,9 +1,11 @@
 import system from "fs";
+import type { QueryKind } from "@persistence/types";
 
 export type Dependencies = {
   system: typeof system;
+  query: QueryKind;
 };
 
-export const dependencies: Dependencies = {
+export const dependencies: Omit<Dependencies, RuntimeSpecified<"query">> = {
   system,
 };
