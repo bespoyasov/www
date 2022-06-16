d30f34e40bbf69eb873ce4010ff43205f4c145c8
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-16 10:34:42 +0200

    Use utils for the post and posts implementation

--- src/services/_persistence/post/post.ts
+++ src/services/_persistence/post/post.ts
@@ -2,13 +2,15 @@ import type { QueryKind } from "../types";
 import type { Dependencies } from "../dependencies";
 import type { QueryPost } from "../ports";
 
+import { directoryFor, withMdx } from "../utils";
+
 type QueryFactory = (query: QueryKind) => QueryPost;
 
 export const createPostQueryFactory: Factory<QueryFactory, Dependencies> =
   ({ path, system }) =>
   (query) =>
   (postId) => {
-    const directory = query;
-    const fileName = postId;
+    const directory = directoryFor(query);
+    const fileName = withMdx(postId);
     return system.readFileSync(path.join(directory, fileName), "utf-8");
   };
--- src/services/_persistence/posts/posts.ts
+++ src/services/_persistence/posts/posts.ts
@@ -2,15 +2,19 @@ import type { QueryKind } from "../types";
 import type { Dependencies } from "../dependencies";
 import type { QueryPosts } from "../ports";
 
+import { directoryFor, isMdx } from "../utils";
+
 type QueryCreator = (query: QueryKind) => QueryPosts;
 
 export const createPostsQueryFactory: Factory<QueryCreator, Dependencies> =
   ({ system, path }) =>
   (query) =>
   () => {
+    const directory = directoryFor(query);
     const posts = system
-      .readdirSync(query)
-      .map((fileName) => system.readFileSync(path.join(query, fileName), "utf-8"));
+      .readdirSync(directory)
+      .filter(isMdx)
+      .map((fileName) => system.readFileSync(path.join(directory, fileName), "utf-8"));
 
     return posts;
   };
