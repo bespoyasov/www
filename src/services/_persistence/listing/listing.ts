3dcaecf91ee70fc5ce5665b5a0c954c582a457c1
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-16 10:31:07 +0200

    Use utils for the correct implementation of listing query

--- src/services/_persistence/listing/listing.ts
+++ src/services/_persistence/listing/listing.ts
@@ -2,10 +2,15 @@ import type { QueryKind } from "../types";
 import type { Dependencies } from "../dependencies";
 import type { QueryListing } from "../ports";
 
+import { directoryFor, isMdx } from "../utils";
+
 type QueryCreator = (query: QueryKind) => QueryListing;
 
 export const createListingQueryFactory: Factory<QueryCreator, Dependencies> =
   ({ system }) =>
   (query) =>
   () =>
-    system.readdirSync(query).map((fileName) => fileName.replace(".mdx", ""));
+    system
+      .readdirSync(directoryFor(query))
+      .filter(isMdx)
+      .map((fileName) => fileName.replace(".mdx", ""));
