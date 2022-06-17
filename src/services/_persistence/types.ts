f58adf71ad99b3c410dfb6a6e5d58d422770ada8
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-17 10:10:18 +0200

    Migrate `persistence` types

---
+++ src/services/_persistence/types.ts
@@ -0,0 +1,7 @@
+export type FileExtension = "mdx" | "tsx";
+export type FileName = AbsolutePath;
+export type FileContent = LocalizedString;
+
+export type ContentDirectory = AbsolutePath;
+
+export type QueryKind = "notes" | "projects" | "talks";
