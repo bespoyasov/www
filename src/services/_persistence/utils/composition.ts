d248755647261b9c0b6358320582f31bbd02fe83
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-17 10:32:31 +0200

    Register the default `isMdx` implementation

--- src/services/_persistence/utils/composition.ts
+++ src/services/_persistence/utils/composition.ts
@@ -1,4 +1,7 @@
 import { dependencies } from "@_persistence/composition";
+
 import { createPathBuilder } from "./directory";
+import { createExtensionChecker } from "./hasExtension";
 
 export const directoryFor = createPathBuilder(dependencies);
+export const isMdx = createExtensionChecker("mdx");
