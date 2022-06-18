1513bfad740d6c1f3025c8456d02c87673d8cdd4
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-18 12:16:54 +0200

    Specify the network service ports

---
+++ src/services/_network/ports.ts
@@ -0,0 +1,7 @@
+import type { Metadata } from "@core/metadata";
+import type { PostId } from "@core/post";
+import type { PostContents } from "@_network/types";
+
+export type FetchContent = (id: PostId) => Promise<PostContents>;
+export type FetchListing = () => List<PostId>;
+export type FetchMetadata = () => List<Metadata>;
