607e52d8565377115d438f295b20bf1d99d16cfd
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-19 12:46:23 +0200

    Register module dependencies

---
+++ src/services/_network/composition.ts
@@ -0,0 +1,19 @@
+import type { Dependencies } from "@_network/dependencies";
+
+import parse from "gray-matter";
+import { serialize } from "next-mdx-remote/serialize";
+
+import { serializationConfig } from "@configs/serialization";
+import { castTo } from "@utils/castTo";
+
+const settings = {
+  serialize: {
+    mdxOptions: castTo(serializationConfig),
+  },
+};
+
+export const dependencies: Dependencies = {
+  serialize,
+  parse,
+  settings,
+};
