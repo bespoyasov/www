ec5f1b33e444a2056d606bf9164abc7c35d492cf
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-19 13:01:03 +0200

    Register the default `content` implementation

---
+++ src/services/_network/content/composition.ts
@@ -0,0 +1,10 @@
+import { getNote, getProject, getTalk } from "@persistence/post";
+
+import { dependencies } from "@_network/composition";
+import { createFetchContentFactory } from "./content";
+
+const contentFor = createFetchContentFactory(dependencies);
+
+export const fetchProject = contentFor(getProject);
+export const fetchNote = contentFor(getNote);
+export const fetchTalk = contentFor(getTalk);
