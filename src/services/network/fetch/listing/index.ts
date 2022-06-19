4f1613fec35ff5028c96bf2dec181d075a2d8f2e
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-19 13:11:03 +0200

    Remove the original network implementation

--- src/services/network/fetch/listing/index.ts
+++
@@ -1,8 +0,0 @@
-import type { PostId } from "@core/post";
-import { noteList, projectList, talkList } from "@persistence/listing";
-
-type FetchNames = () => List<PostId>;
-
-export const projectNames: FetchNames = projectList;
-export const noteNames: FetchNames = noteList;
-export const talkNames: FetchNames = talkList;
