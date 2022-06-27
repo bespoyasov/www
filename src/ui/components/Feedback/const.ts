0c4ce280bc0b83e9339e2210cc3f4335442241c3
Alexander Bespoyasov <forjob159@gmail.com>
2022-06-27 22:30:05 +0200

    Add a link to the GitHub repo

--- src/ui/components/Feedback/const.ts
+++ src/ui/components/Feedback/const.ts
@@ -1,2 +1,4 @@
 export const FACEBOOK_TEMPLATE = "https://facebook.com/sharer.php?u={URL}";
 export const TWITTER_TEMPLATE = "https://twitter.com/intent/tweet?text={TITLE}&url={URL}";
+
+export const GITHUB_LINK = "https://github.com/bespoyasov/www";
