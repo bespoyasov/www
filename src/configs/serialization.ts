bbb26fb79a5b293ffb9495e93b8b7a023a58ca71
Alex Bespoyasov <bespoyasov@me.com>
2023-02-27 09:02:40 +0100

    Revert hardcoded `development` flag [skip ci]
    Introduced and more info in:
    https://github.com/bespoyasov/www/commit/47afebdf2dd787be994e5ae548f583d9767c3586

--- src/configs/serialization.ts
+++ src/configs/serialization.ts
@@ -8,7 +8,6 @@ import syntaxHighlight from "rehype-highlight";
 import fsharp from "highlight.js/lib/languages/fsharp";
 
 export const serializationConfig = {
-  development: false,
   remarkPlugins: [
     githubFlavor,
     unwrapImages,
