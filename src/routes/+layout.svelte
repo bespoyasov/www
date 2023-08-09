5656ac33e3aa3ee56efbe77fdf6614b767068973
Alex Bespoyasov <bespoyasov@me.com>
2023-08-09 17:07:01 +0200

    Specify layout geometry

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,26 @@
 	import '../styles/global.css';
 </script>
 
-<slot />
+<div class="layout">
+	<div class="content">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.layout {
+		margin: auto;
+		padding-inline: 1.5rem;
+
+		display: grid;
+		grid-template-rows: auto 1fr auto;
+
+		max-inline-size: 1100px;
+		min-block-size: 100vh;
+		min-block-size: 100dvh;
+	}
+
+	.content {
+		min-inline-size: 0;
+	}
+</style>
