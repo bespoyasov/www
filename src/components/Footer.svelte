bba6a87c42ac5618669774dfe0a53c19adec767d
Alex Bespoyasov <bespoyasov@me.com>
2023-08-09 17:43:41 +0200

    Add `Footer` component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,53 @@
+<script context="module">
+	import { locale } from '../lib/config';
+	import { author } from '../lib/author';
+
+	const t = {
+		en: {
+			title: 'Contacts',
+			github: 'GitHub',
+			twitter: 'Twitter',
+			linkedin: 'LinkedIn',
+			employer: '0+X'
+		},
+		ru: {
+			title: 'Контакты',
+			twitter: 'Твитер',
+			github: 'Гитхаб',
+			linkedin: 'Линкед',
+			employer: '0+X'
+		}
+	}[locale];
+</script>
+
+<footer>
+	<div>
+		<h2 class="visually-hidden">{t.title}</h2>
+		<ul class="reset">
+			<li><a href={author.twitter}>{t.twitter}</a></li>
+			<li><a href={author.github}>{t.github}</a></li>
+			<li><a href={author.linkedin}>{t.linkedin}</a></li>
+			<li><a href={author.employer}>{t.employer}</a></li>
+		</ul>
+	</div>
+
+	<div class="since">2010+</div>
+</footer>
+
+<style>
+	footer {
+		padding-block: 1.5rem;
+		display: flex;
+		justify-content: space-between;
+		align-items: baseline;
+	}
+
+	ul {
+		display: flex;
+		gap: 0.5em;
+	}
+
+	.since {
+		font-size: var(--fs-smaller);
+	}
+</style>
