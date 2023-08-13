5871227b8fdd132c5a558638b4be2b3e4904b6b8
Alex Bespoyasov <bespoyasov@me.com>
2023-08-13 19:19:13 +0200

    Add projects page draft

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,1 +1,37 @@
-<h1>Projects</h1>
+<script>
+	import Meta from '../../components/Meta.svelte';
+
+	export let data;
+
+	const { t } = data;
+</script>
+
+<Meta metadata={t} />
+
+<main>
+	<section>
+		<h1>{t.title}</h1>
+		<p>{t.intro}</p>
+	</section>
+
+	<section>
+		<h2>{t.archive.title}</h2>
+		<p>{t.archive.about}</p>
+	</section>
+
+	<section>
+		<h2>{t.misc.title}</h2>
+		<p>{t.misc.about}</p>
+	</section>
+
+	<section>
+		<h2>{t.limb.title}</h2>
+		<p>{t.limb.about}</p>
+	</section>
+</main>
+
+<style>
+	section {
+		margin-block-end: 4rem;
+	}
+</style>
