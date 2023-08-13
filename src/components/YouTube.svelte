d293bd4afef7c4e374ff79456765e1279afaca16
Alex Bespoyasov <bespoyasov@me.com>
2023-08-13 10:39:44 +0200

    Add `YouTube` component

---
+++ src/components/YouTube.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let src;
+	export let caption;
+</script>
+
+<figure class="youtube">
+	<iframe {src} title={caption} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen />
+
+	{#if caption}
+		<figcaption>{caption}</figcaption>
+	{/if}
+</figure>
