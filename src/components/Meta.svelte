d5738a30ab434d3dd146e9958f7648900a710c8f
Alex Bespoyasov <bespoyasov@me.com>
2023-08-13 11:02:11 +0200

    Extend `Meta` with social media cards and previews

--- src/components/Meta.svelte
+++ src/components/Meta.svelte
@@ -1,12 +1,34 @@
 <script context="module">
 	import { site } from '../lib/site';
+	import { author } from '../lib/author';
+
+	const handle = '@' + author.twitter.substring(author.twitter.lastIndexOf('/') + 1);
 </script>
 
 <script>
 	export let metadata;
+
+	const { title, description, cover, slug } = metadata;
+	const preview = !!cover && `${site.url}/images/notes/${slug}/${cover}`;
 </script>
 
 <svelte:head>
-	<title>{metadata.title} • {site.title}</title>
-	<meta name="description" content={metadata.description} />
+	<title>{title} • {site.title}</title>
+	<meta name="description" content={description} />
+
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	{#if preview}<meta property="og:image" content={preview} />{/if}
+
+	<meta name="twitter:site" content={handle} />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+
+	{#if preview}
+		<meta name="twitter:card" content="summary_large_image" />
+		<meta name="twitter:image" content={preview} />
+	{:else}
+		<meta name="twitter:card" content="summary" />
+	{/if}
 </svelte:head>
