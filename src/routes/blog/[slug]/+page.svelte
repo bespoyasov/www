32c2d1013e220f541c680bf521a5d0193c520164
Alex Bespoyasov <bespoyasov@me.com>
2023-08-13 11:08:18 +0200

    Create post page

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Post from '../../../components/Post.svelte';
+	import Meta from '../../../components/Meta.svelte';
+
+	export let data;
+</script>
+
+<svelte:head>
+	<script type="module" src="/scripts/code-sample-switch.js"></script>
+</svelte:head>
+
+<Meta metadata={data.meta} />
+<Post {data} />
