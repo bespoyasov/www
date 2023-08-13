64febfff77a55395a6b1ac3f3692d99751f95d50
Alex Bespoyasov <bespoyasov@me.com>
2023-08-13 19:36:15 +0200

    Add tags page draft

--- src/routes/tags/+page.svelte
+++ src/routes/tags/+page.svelte
@@ -1,1 +1,34 @@
-<h1>Tags</h1>
+<script>
+	import Meta from '../../components/Meta.svelte';
+
+	export let data;
+
+	const { t } = data;
+</script>
+
+<Meta metadata={t} />
+
+<main class="tags">
+	<header class="header">
+		<h1>{t.title}</h1>
+		<p>{t.description}</p>
+	</header>
+</main>
+
+<style>
+	.header {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		align-items: baseline;
+		gap: 0 1em;
+	}
+
+	.header h1 {
+		width: 100%;
+	}
+
+	.header p {
+		margin-block-end: 0.5em;
+	}
+</style>
