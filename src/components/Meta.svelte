c5eb695a2c23cfa97fe7475dadb26ec43c9cfbd1
Alex Bespoyasov <bespoyasov@me.com>
2023-08-13 10:12:31 +0200

    Extract `head` contents builder

---
+++ src/components/Meta.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+	import { site } from '../lib/site';
+</script>
+
+<script>
+	export let metadata;
+</script>
+
+<svelte:head>
+	<title>{metadata.title} • {site.title}</title>
+	<meta name="description" content={metadata.description} />
+</svelte:head>
