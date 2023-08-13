20c33ca6bf8881e09e20da998dd670d63a2a67d7
Alex Bespoyasov <bespoyasov@me.com>
2023-08-13 19:23:08 +0200

    Enrich projects with additional decorative data

--- src/routes/projects/+page.server.js
+++ src/routes/projects/+page.server.js
@@ -1,4 +1,5 @@
 import { fetchPosts } from '../../lib/fetch-posts';
+import { decorateProjects } from '../../lib/decorate-projects';
 
 import { locale } from '../../lib/config';
 import translations from './translations.json';
@@ -6,7 +7,8 @@ import translations from './translations.json';
 export async function load() {
 	const posts = fetchPosts({ kind: 'projects' });
 
-	const prepare = (category) => posts.filter((item) => item.category === category);
+	const prepare = (category) =>
+		decorateProjects(posts.filter((item) => item.category === category));
 
 	return {
 		projects: prepare('live'),
