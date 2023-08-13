ac61d774fc6c7962d985b08a1068caf8f5e4f25c
Alex Bespoyasov <bespoyasov@me.com>
2023-08-13 11:07:28 +0200

    Add `Post` component draft

---
+++ src/components/Post.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let data;
+</script>
+
+<main class="post">
+	{@html data.content}
+</main>
+
+<style>
+	.post {
+		margin-block-end: 1em;
+	}
+
+	@media (min-width: 800px) {
+		.post {
+			inline-size: 75%;
+			max-inline-size: 800px;
+		}
+	}
+</style>
