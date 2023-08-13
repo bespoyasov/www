9a56f01c2e982c46a5dd6027a230897e4f0e2f31
Alex Bespoyasov <bespoyasov@me.com>
2023-08-13 19:25:04 +0200

    Render projects on their page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Meta from '../../components/Meta.svelte';
+	import ProjectList from '../../components/ProjectList.svelte';
 
 	export let data;
 
@@ -12,21 +13,25 @@
 	<section>
 		<h1>{t.title}</h1>
 		<p>{t.intro}</p>
+		<ProjectList projects={data.projects} />
 	</section>
 
 	<section>
 		<h2>{t.archive.title}</h2>
 		<p>{t.archive.about}</p>
+		<ProjectList projects={data.archived} />
 	</section>
 
 	<section>
 		<h2>{t.misc.title}</h2>
 		<p>{t.misc.about}</p>
+		<ProjectList projects={data.misc} />
 	</section>
 
 	<section>
 		<h2>{t.limb.title}</h2>
 		<p>{t.limb.about}</p>
+		<ProjectList projects={data.limb} />
 	</section>
 </main>
 
