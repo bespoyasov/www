b71db7999a68322bd175345f95ecd8073421df73
Alex Bespoyasov <bespoyasov@me.com>
2023-08-13 19:30:04 +0200

    Render talks on page

--- src/routes/talks/+page.server.js
+++ src/routes/talks/+page.server.js
@@ -1,7 +1,9 @@
+import { fetchPosts } from '../../lib/fetch-posts';
 import { locale } from '../../lib/config';
 import translations from './translations.json';
 
 export async function load() {
+	const talks = fetchPosts({ kind: 'talks' });
 	const t = translations[locale];
-	return { t };
+	return { t, talks };
 }
