466e5518c50bb7954b3704c466c15f15e6f2b32e
Alex Bespoyasov <bespoyasov@me.com>
2023-08-13 11:04:06 +0200

    Override resulting `html`
    Ensure correct `img` src  paths and include rendered styles.

--- src/lib/fetch-content.js
+++ src/lib/fetch-content.js
@@ -3,10 +3,14 @@ import { locale } from './config';
 export async function fetchContent({ kind, slug }) {
 	const module = await import(`../../storage/${kind}/${slug}/${locale}.md`);
 	const { metadata, default: content } = module;
-	const { html } = content.render();
+	const { html, css } = content.render();
+
+	const overridden = html
+		.replaceAll('src="./', `src="/images/${kind}/${slug}/`)
+		.concat(`<style>${css.code}</style>`);
 
 	return {
 		meta: metadata,
-		html
+		html: overridden
 	};
 }
