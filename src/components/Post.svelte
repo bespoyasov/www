fc0045b3a6f5cd8ed6382ce1d68c888b34c9cb85
Alex Bespoyasov <bespoyasov@me.com>
2023-08-13 11:09:56 +0200

    Render post metadata

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -6,13 +6,29 @@
 	{@html data.content}
 </main>
 
+<footer class="footer">
+	<time datetime={data.meta.datetime}>{data.meta.date.full}</time>
+
+	{#each data.meta.tags as tag}
+		<a class="text" href={`/tags/${tag}`}>#{tag}</a>
+	{/each}
+</footer>
+
 <style>
 	.post {
 		margin-block-end: 1em;
 	}
 
+	.footer {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 0 1em;
+		font-size: var(--fs-smaller);
+	}
+
 	@media (min-width: 800px) {
-		.post {
+		.post,
+		.footer {
 			inline-size: 75%;
 			max-inline-size: 800px;
 		}
