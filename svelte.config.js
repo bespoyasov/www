f018ae3d8cd0b639ef9d5adfac0c6bfcfdf91aff
Alex Bespoyasov <bespoyasov@me.com>
2023-08-13 10:05:56 +0200

    Set up `mdsvex` in configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	extensions: ['.svelte', '.md'],
+	preprocess: [
+		mdsvex({
+			extensions: ['.md']
+		})
+	]
 };
 
 export default config;
