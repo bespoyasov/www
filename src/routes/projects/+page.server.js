a7414e4d072067118914834eb1ae6c0a7ce6ed88
Alex Bespoyasov <bespoyasov@me.com>
2023-08-13 19:20:36 +0200

    Provide page data for all sections

--- src/routes/projects/+page.server.js
+++ src/routes/projects/+page.server.js
@@ -1,8 +1,18 @@
+import { fetchPosts } from '../../lib/fetch-posts';
+
 import { locale } from '../../lib/config';
 import translations from './translations.json';
 
 export async function load() {
+	const posts = fetchPosts({ kind: 'projects' });
+
+	const prepare = (category) => posts.filter((item) => item.category === category);
+
 	return {
+		projects: prepare('live'),
+		archived: prepare('archived'),
+		misc: prepare('misc'),
+		limb: prepare('limb'),
 		t: translations[locale]
 	};
 }
