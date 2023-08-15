1ac2a238537295f65cf6edce0998d43de0ea83d1
Alex Bespoyasov <bespoyasov@me.com>
2023-08-15 16:33:55 +0200

    Provide tags page data

---
+++ src/lib/fetch-tags.js
@@ -0,0 +1,16 @@
+import { loaders } from './meta';
+
+export function fetchTags() {
+	const tags = Object.values(loaders)
+		.map((load) => Object.values(load()))
+		.flat()
+		.map((file) => file.metadata.tags)
+		.flat()
+		.reduce((acc, tag) => {
+			acc[tag] ??= 0;
+			acc[tag] += 1;
+			return acc;
+		}, {});
+
+	return tags;
+}
--- src/routes/tags/+page.server.js
+++ src/routes/tags/+page.server.js
@@ -1,8 +1,16 @@
+import { fetchTags } from '../../lib/fetch-tags';
+import { groupByName } from '../../lib/group-by-name';
+import { groupByFrequency } from '../../lib/group-by-frequency';
+
 import { locale } from '../../lib/config';
 import translations from './translations.json';
 
 export async function load() {
+	const tags = fetchTags();
+
 	return {
+		tagsByName: groupByName(tags),
+		tagsByFrequency: groupByFrequency(tags),
 		t: translations[locale]
 	};
 }
