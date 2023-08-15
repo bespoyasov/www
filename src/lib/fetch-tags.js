7ff9c29b9ab1b9737f8e98366d1191ef044417c3
Alex Bespoyasov <bespoyasov@me.com>
2023-08-15 16:56:49 +0200

    Use nullish coallesing instead of another filter

--- src/lib/fetch-tags.js
+++ src/lib/fetch-tags.js
@@ -4,9 +4,8 @@ export function fetchTags() {
 	const tags = Object.values(loaders)
 		.map((load) => Object.values(load()))
 		.flat()
-		.map((file) => file.metadata.tags)
+		.map((file) => file.metadata.tags ?? [])
 		.flat()
-		.filter(Boolean)
 		.reduce((acc, tag) => {
 			acc[tag] ??= 0;
 			acc[tag] += 1;
