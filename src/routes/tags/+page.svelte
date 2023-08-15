e4f9de80f83269c96742d05e31d0fc0098b756e3
Alex Bespoyasov <bespoyasov@me.com>
2023-08-15 16:39:29 +0200

    Specify basic content grid and render tag list

--- src/routes/tags/+page.svelte
+++ src/routes/tags/+page.svelte
@@ -3,7 +3,10 @@
 
 	export let data;
 
-	const { t } = data;
+	const { t, tagsByName, tagsByFrequency } = data;
+
+	const groups = [tagsByName, tagsByFrequency];
+	const options = Object.keys(t.labels);
 </script>
 
 <Meta metadata={t} />
@@ -13,6 +16,24 @@
 		<h1>{t.title}</h1>
 		<p>{t.description}</p>
 	</header>
+
+	{#each groups as group, index}
+		{@const shown = options[index]}
+		<div class="content" data-group={shown} hidden={index > 0}>
+			{#each group as [title, tags]}
+				<section>
+					<h2>{title}</h2>
+					<ul>
+						{#each tags as [tag, used]}
+							<li>
+								<a href={`/tags/${tag}`}>#{tag}</a><sup class="used">&thinsp;{used}</sup>
+							</li>
+						{/each}
+					</ul>
+				</section>
+			{/each}
+		</div>
+	{/each}
 </main>
 
 <style>
@@ -30,5 +51,44 @@
 
 	.header p {
 		margin-block-end: 0.5em;
+	}
+
+	.tags h2,
+	.tags ul {
+		margin-block: 0;
+	}
+
+	.used {
+		color: var(--color-dimmed);
+	}
+
+	.content {
+		--columns: 1;
+
+		margin-block: 1.5em;
+	}
+
+	.content:not([hidden]) {
+		display: grid;
+		grid-template-columns: repeat(var(--columns), 1fr);
+		gap: 2.5em;
+	}
+
+	@media (min-width: 400px) {
+		.content {
+			--columns: 2;
+		}
+	}
+
+	@media (min-width: 800px) {
+		.content {
+			--columns: 3;
+		}
+	}
+
+	@media (min-width: 1000px) {
+		.content {
+			--columns: 4;
+		}
 	}
 </style>
