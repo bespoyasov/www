43ae2f2c3a468b3793bbe47c6ae16cb303449647
Alex Bespoyasov <bespoyasov@me.com>
2023-08-15 17:05:06 +0200

    Add main page

---
+++ src/routes/+page.server.js
@@ -0,0 +1,27 @@
+import { fetchPosts } from '../lib/fetch-posts';
+import { fetchTags } from '../lib/fetch-tags';
+
+import { locale } from '../lib/config';
+import translations from './translations.json';
+
+const MAX_NOTES = 5;
+const MAX_TALKS = 3;
+const MAX_TAGS = 10;
+
+export async function load() {
+	const t = translations[locale];
+
+	const projects = fetchPosts({ kind: 'projects' }).filter((x) => x.featured);
+	const notes = fetchPosts({ kind: 'notes' }).slice(0, MAX_NOTES);
+
+	const talks = fetchPosts({ kind: 'talks' })
+		.filter((talk) => talk.materials.includes('video') && talk.materials.includes('sources'))
+		.slice(0, MAX_TALKS);
+
+	const tags = Object.entries(fetchTags())
+		.sort((a, b) => b[1] - a[1])
+		.map(([tag]) => tag)
+		.slice(0, MAX_TAGS);
+
+	return { projects, notes, talks, tags, t };
+}
