e49c427cf9a42dfb508d49752627c0fff07f2f8a
Alex Bespoyasov <bespoyasov@me.com>
2023-08-15 16:40:45 +0200

    Add grouping method switcher

--- src/routes/tags/+page.svelte
+++ src/routes/tags/+page.svelte
@@ -7,6 +7,7 @@
 
 	const groups = [tagsByName, tagsByFrequency];
 	const options = Object.keys(t.labels);
+	const selected = options.at(0);
 </script>
 
 <Meta metadata={t} />
@@ -15,6 +16,22 @@
 	<header class="header">
 		<h1>{t.title}</h1>
 		<p>{t.description}</p>
+
+		<form class="group">
+			{#each options as value}
+				{@const label = data.t.labels[value]}
+				<label>
+					<input
+						type="radio"
+						name="order"
+						class="visually-hidden"
+						checked={value === selected}
+						{value}
+					/>
+					<span>{label}</span>
+				</label>
+			{/each}
+		</form>
 	</header>
 
 	{#each groups as group, index}
@@ -66,9 +83,7 @@
 		--columns: 1;
 
 		margin-block: 1.5em;
-	}
 
-	.content:not([hidden]) {
 		display: grid;
 		grid-template-columns: repeat(var(--columns), 1fr);
 		gap: 2.5em;
@@ -90,5 +105,54 @@
 		.content {
 			--columns: 4;
 		}
+	}
+
+	/* Radio Group. */
+
+	.group {
+		display: flex;
+		-webkit-user-select: none;
+		user-select: none;
+	}
+
+	.group label {
+		margin-inline-end: -1px;
+		white-space: nowrap;
+	}
+
+	.group span {
+		padding: 0.025em 0.5em;
+		border: 1px solid var(--color-adornment);
+		cursor: pointer;
+	}
+
+	.group span:hover {
+		color: var(--color-accents);
+	}
+
+	.group :focus-within span {
+		outline: 5px auto Highlight;
+		outline: 5px auto -webkit-focus-ring-color;
+	}
+
+	.group :checked + span {
+		color: var(--color-foreground);
+		background-color: var(--color-adornment);
+		cursor: default;
+	}
+
+	.group :first-child span {
+		border-radius: 5px 0 0 5px;
+	}
+
+	.group :last-child span {
+		border-radius: 0 5px 5px 0;
+	}
+
+	/* Group ~ Content binding. */
+
+	.header:has([value='alphabet']:checked) ~ [data-group='frequency'],
+	.header:has([value='frequency']:checked) ~ [data-group='alphabet'] {
+		display: none;
 	}
 </style>
