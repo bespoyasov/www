4d0cb8cc22583f9859de88d5df1de24cb07799a4
Alex Bespoyasov <bespoyasov@me.com>
2023-08-15 19:36:54 +0200

    Add screenshot tests for pages

---
+++ tests/pages.js
@@ -0,0 +1,10 @@
+export const pages = [
+	{ path: '/', name: 'Main Page' },
+	{ path: '/projects', name: 'Projects Page' },
+	{ path: '/talks', name: 'Talks Page' },
+	{ path: '/tags', name: 'Tags Page' },
+	{ path: '/blog', name: 'Blog Page' },
+	{ path: '/blog/text-generation-with-markov-chains/', name: 'Text Generation Post' },
+	{ path: '/blog/domain-modelling-made-functional-3/', name: 'Book Summary Post' },
+	{ path: '/blog/clean-architecture-on-frontend/', name: 'Clean Architecture Post' }
+];
